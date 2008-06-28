a0d60014b667f5aae91d91783bdc268ea4bed7ee
Ryan Curtin <ryan@igglybob.com>
2008-06-28 18:46:22 +0000

    Fixes #30; game no longer crashes in network mode.  I took BitPacking.h from [30] and did the re-indenting myself since it seemed as though the scripted indent work messed things up.

--- NetTarget/Util/BitPacking.h
+++ NetTarget/Util/BitPacking.h
@@ -4,16 +4,14 @@
 #define BIT_PACKING_H
 
 #ifdef MR_UTIL
-#define MR_DllDeclare   __declspec( dllexport )
+#define MR_DllDeclare   __declspec(dllexport)
 #else
-#define MR_DllDeclare   __declspec( dllimport )
+#define MR_DllDeclare   __declspec(dllimport)
 #endif
 
 #include "MR_Types.h"
 
-class MR_BitPack
-{
-
+class MR_BitPack {
 	protected:
 		MR_UInt8 mData[1];
 
@@ -28,8 +26,7 @@ class MR_BitPack
 // Inlined because good performances are needed
 inline MR_Int32 MR_BitPack::Get(int pOffset, int pLen, int pPrecision) const
 {
-	union
-	{
+	union {
 		MR_Int32 s;
 		MR_UInt32 u;
 	} lReturnValue;
@@ -39,10 +36,12 @@ inline MR_Int32 MR_BitPack::Get(int pOffset, int pLen, int pPrecision) const
 	if((pLen + (pOffset % 8)) > 32) {
 		lReturnValue.u |= (*(MR_UInt32 *) (mData + (pOffset / 8) + 4)) << (32 - (pOffset % 8));
 	}
-	// Clear overhead bits and preserve sign lReturnValue.s = lReturnValue.s << (32 - pLen) >> (32 - pLen);
+	// Clear overhead bits and preserve sign
+	lReturnValue.s = lReturnValue.s << (32 - pLen) >> (32 - pLen);
 
 	return lReturnValue.s << pPrecision;
 }
+
 
 inline MR_UInt32 MR_BitPack::Getu(int pOffset, int pLen, int pPrecision) const
 {
@@ -53,7 +52,8 @@ inline MR_UInt32 MR_BitPack::Getu(int pOffset, int pLen, int pPrecision) const
 	if(pLen + (pOffset % 8) > 32) {
 		lReturnValue |= (*(MR_UInt32 *) (mData + (pOffset / 8) + 4)) << (32 - (pOffset % 8));
 	}
-	// Clear overhead bits lReturnValue = lReturnValue << (32 - pLen) >> (32 - pLen);
+	// Clear overhead bits
+	lReturnValue = lReturnValue << (32 - pLen) >> (32 - pLen);
 
 	return lReturnValue << pPrecision;
 }
@@ -65,7 +65,6 @@ inline void MR_BitPack::Clear(int pSize)
 
 inline void MR_BitPack::Set(int pOffset, int pLen, int pPrecision, MR_Int32 pValue)
 {
-
 	MR_UInt32 lValue = pValue >> pPrecision;
 
 	lValue = lValue << (32 - pLen) >> (32 - pLen);
