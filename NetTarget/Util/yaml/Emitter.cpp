d5c152d6af9dcc16fe70ba9333a103addf8e0f61
Michael Imamura <zoogie@lugatgt.org>
2008-06-29 01:38:00 +0000

    Added yaml::Emitter utility class to support #21.
    Also added Util/yaml directory to Doxyfile.

---
+++ NetTarget/Util/yaml/Emitter.cpp
@@ -0,0 +1,171 @@
+
+/* Emitter.cpp
+	Implementation for the LibYAML emitter wrapper. */
+
+#include "../stdafx.h"
+
+#include "Emitter.h"
+
+#include <yaml.h>
+
+using namespace yaml;
+
+#define YAML_STR(s) ((yaml_char_t*)(s))
+
+static const std::string trueStr("true");
+static const std::string falseStr("false");
+
+/**
+ * Create a new emitter.
+ * @param file An open file for writing (may not be null).
+ *             It is the caller's job to close the file when the emitter
+ *             is destroyed.
+ */
+Emitter::Emitter(FILE *file)
+{
+	yaml_event_t event;
+
+	// Initialize the LibYAML emitter.
+	memset(&emitter, 0, sizeof(emitter));
+	yaml_emitter_initialize(&emitter);
+	yaml_emitter_set_output_file(&emitter, file);
+
+	// Start the stream.
+	yaml_stream_start_event_initialize(&event, YAML_UTF8_ENCODING);
+	if(!yaml_emitter_emit(&emitter, &event))
+		throw EmitterExn("Unable to start stream");
+
+	yaml_version_directive_t version;
+	version.major = 1;
+	version.minor = 1;
+
+	yaml_tag_directive_t nullTag;
+	nullTag.handle = NULL;
+	nullTag.prefix = NULL;
+
+	// Start the document.
+	yaml_document_start_event_initialize(&event, &version, &nullTag, &nullTag, 1);
+	if(!yaml_emitter_emit(&emitter, &event))
+		throw EmitterExn("Unable to start document");
+}
+
+/// Destructor.
+Emitter::~Emitter()
+{
+	yaml_event_t event;
+
+	// End the document.
+	yaml_document_end_event_initialize(&event, 1);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		yaml_emitter_delete(&emitter);
+		throw EmitterExn("Unable to end document");
+	}
+
+	// End the stream.
+	yaml_stream_end_event_initialize(&event);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		yaml_emitter_delete(&emitter);
+		throw EmitterExn("Unable to end stream");
+	}
+
+	yaml_emitter_delete(&emitter);
+}
+
+/**
+ * Start a map.
+ * Writing a map involves:
+ * - First, call StartMap().
+ * - For each key-value pair, first write the key with MapKey(),
+ *   then write the value (e.g. Value(), another map, a sequence, etc.)
+ * - Finally, call EndMap() to close the map.
+ */
+void Emitter::StartMap()
+{
+	yaml_event_t event;
+	yaml_mapping_start_event_initialize(&event, NULL, NULL, 1, YAML_BLOCK_MAPPING_STYLE);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		throw EmitterExn("Unable to start map");
+	}
+}
+
+/**
+ * Write the key of a map.
+ * @param s The key name (may not be empty).
+ */
+void Emitter::MapKey(const std::string &s)
+{
+	Value(s);
+}
+
+/// End the current map.
+void Emitter::EndMap()
+{
+	yaml_event_t event;
+	yaml_mapping_end_event_initialize(&event);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		throw EmitterExn("Unable to start map");
+	}
+}
+
+/**
+ * Write a single string value.
+ * @param val The value.
+ */
+void Emitter::Value(const std::string &val)
+{
+	yaml_event_t event;
+	yaml_scalar_event_initialize(&event, NULL, NULL,
+		YAML_STR(val.c_str()), val.length(),
+		1, 1, YAML_ANY_SCALAR_STYLE);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		throw EmitterExn("Unable to write scalar value");
+	}
+}
+
+/**
+ * Write a single string value.
+ * @param val The value (may not be NULL).
+ */
+void Emitter::Value(const char *val)
+{
+	yaml_event_t event;
+	yaml_scalar_event_initialize(&event, NULL, NULL,
+		YAML_STR(val), strlen(val),
+		1, 1, YAML_ANY_SCALAR_STYLE);
+	if(!yaml_emitter_emit(&emitter, &event)) {
+		throw EmitterExn("Unable to write scalar value");
+	}
+}
+
+/**
+ * Write a single boolean value.
+ * @param val The value.
+ */
+void Emitter::Value(bool val)
+{
+	Value(val ? trueStr : falseStr);
+}
+
+/**
+ * Write a single integer value.
+ * @param val The value.
+ */
+void Emitter::Value(int val)
+{
+	char buf[256];
+	_snprintf(buf, 255, "%d", val);
+	buf[255] = 0;
+	Value(buf);
+}
+
+/**
+ * Write a single floating-point value.
+ * @param val The value.
+ */
+void Emitter::Value(double val)
+{
+	char buf[256];
+	_snprintf(buf, 255, "%g", val);
+	buf[255] = 0;
+	Value(buf);
+}
---
+++ NetTarget/Util/yaml/Emitter.h
@@ -0,0 +1,46 @@
+
+/* yaml/Emitter.h
+	Header for yaml::Emitter. */
+
+#pragma once
+
+#include <exception>
+#include <string>
+
+#include <yaml.h>
+
+namespace yaml
+{
+	// Standard exception thrown for emitter errors.
+	class EmitterExn : public std::exception
+	{
+		typedef std::exception SUPER;
+
+		public:
+			EmitterExn() : SUPER() { }
+			EmitterExn(const char *const &s) : SUPER(s) { }
+	};
+
+	/// Wrapper for the LibYAML emitter.
+	class Emitter
+	{
+		private:
+			Emitter() { }
+		public:
+			Emitter(FILE *file);
+			virtual ~Emitter();
+
+			void StartMap();
+			void MapKey(const std::string &s);
+			void EndMap();
+
+			void Value(const std::string &val);
+			void Value(const char *val);
+			void Value(bool val);
+			void Value(int val);
+			void Value(double val);
+
+		private:
+			yaml_emitter_t emitter;
+	};
+}
