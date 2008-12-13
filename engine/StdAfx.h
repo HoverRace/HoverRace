06cb8d2da431e5d20c4739f827eb9a373d6b8f51
Michael Imamura <zoogie@lugatgt.org>
2008-12-13 08:11:39 +0000

    Use a single shared precompiled header for the engine project.

---
+++ engine/StdAfx.h
@@ -0,0 +1,35 @@
+
+/* StdAfx.h
+	Precompiled header for engine. */
+
+#ifdef _WIN32
+
+#	define VC_EXTRALEAN							  // Exclude rarely-used stuff from Windows headers
+
+	// Minimum Windows version: XP
+#	define WINVER 0x0501
+
+#	include <afxwin.h>								// MFC core and standard components
+#	include <afxext.h>								// MFC extensions
+#	include <afxtempl.h>
+
+#	include <typeinfo.h>
+
+#	ifndef _AFX_NO_AFXCMN_SUPPORT
+#		include <afxcmn.h>							// MFC support for Windows 95 Common Controls
+#	endif											// _AFX_NO_AFXCMN_SUPPORT
+
+#else
+
+#	include "../../include/compat/unix.h"
+
+#endif
+
+#include <stdlib.h>
+
+// Commonly-used STL headers.
+#include <exception>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
