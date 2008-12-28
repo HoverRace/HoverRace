1928621ca383669b230f28940837cd309cfec75a
Michael Imamura <zoogie@lugatgt.org>
2008-12-28 23:01:44 +0000

    Added implementation of Int32x32To64().

--- include/compat/unix.h
+++ include/compat/unix.h
@@ -2,10 +2,15 @@
 /* Compatibility for Win32-specific defines. */
 
 #include <assert.h>
+#include <inttypes.h>
 
 #define ASSERT(e) assert(e)
 
 #define BOOL int
 #define TRUE 1
 #define FALSE 0
+
+#define Int32x32To64(x,y) \
+	static_cast<int64_t>( \
+		static_cast<int64_t>(x) * static_cast<int64_t>(y));
 
