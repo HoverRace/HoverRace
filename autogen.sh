2b1486387bc9f0fbe82cce6ed0a9c19e06a8fd92
Michael Imamura <zoogie@lugatgt.org>
2009-01-31 22:44:22 +0000

    Clear the autom4te cache when version.h is out of date.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,23 @@
-#! /bin/sh
+#!/bin/sh
+
+SELF=`basename "$0"`
+
+MKFILE=build/mkversion.sh
+RCFILE=client/Game2/Game2.rc
+VERFILE=client/Game2/version.h
+
+# Check if we need to invalidate the autom4te cache.
+# This is so that mkversion.sh will be re-run as necessary.
+if [ -e autom4te.cache -a \( \
+	\! -f "$VERFILE" -o \
+	"$0" -nt "$VERFILE" -o \
+	"$RCFILE" -nt "$VERFILE" -o \
+	"$MKFILE" -nt "$VERFILE" \
+	\) ]
+then
+	echo "$SELF: Clearing autom4te cache"
+	rm -rf autom4te.cache
+fi
+
 autoreconf -v --install || exit 1
 ./configure --enable-maintainer-mode "$@"
