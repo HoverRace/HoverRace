29e1e321ee9f05e6ec0071d432e38c2f5af8440b
Michael Imamura <zoogie@lugatgt.org>
2009-02-07 21:48:53 +0000

    #20: Initial test environment for Linux build. Run ./hr to build the "share" directory and launch the binary.

---
+++ build/mklocale.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd `dirname $0`/../share
+
+rm -rf locale
+mkdir locale
+for lingua in "$@"; do
+	lingua=`basename "$lingua" .gmo`
+	gmofile="../po/$lingua.gmo"
+	if [ -f "$gmofile" ]; then
+		echo "$lingua"
+		mkdir "locale/$lingua"
+		mkdir "locale/$lingua/LC_MESSAGES"
+		ln -s \
+			"$gmofile" \
+			"locale/$lingua/LC_MESSAGES/hoverrace.mo"
+	fi
+done
+
