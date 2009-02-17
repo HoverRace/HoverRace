b3b81acda25f6aaaf19075625cddbb651bd939c4
Michael Imamura <zoogie@lugatgt.org>
2009-02-17 02:53:40 +0000

    Added StaticText, an i18n renderable for static chunks of HUD text.

---
+++ engine/VideoServices/StaticText.cpp
@@ -0,0 +1,197 @@
+
+// StaticText.cpp
+// Static text widget with i18n support.
+//
+// Copyright (c) 2009 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../Util/Str.h"
+#include "2DViewport.h"
+
+#include "StaticText.h"
+
+using namespace HoverRace::Util;
+
+using namespace HoverRace::VideoServices;
+
+static RGBQUAD RGB_BLACK = { 0, 0, 0, 0 };
+static RGBQUAD RGB_WHITE = { 0xff, 0xff, 0xff, 0 };
+
+/**
+ * Constructor.
+ * @param s The text (UTF-8).
+ * @param font The font name.
+ * @param size The text size.
+ * @param bold @c true for bold text.
+ * @param italic @c true for italic text.
+ * @param color The color index.
+ */
+StaticText::StaticText(const std::string &s,
+                       const std::string &font,
+                       int size, bool bold, bool italic,
+                       MR_UInt8 color) :
+	s(s), ws(Str::Utf8ToWide(s.c_str())), wsLen(wcslen(ws)),
+	font(font), size(size), bold(bold), italic(italic), color(color),
+	bitmap(NULL), width(0), height(0)
+{
+	Update();
+}
+
+StaticText::~StaticText()
+{
+	free(bitmap);
+	free(ws);
+}
+
+int StaticText::GetWidth() const
+{
+	return width;
+}
+
+int StaticText::GetHeight() const
+{
+	return height;
+}
+
+void StaticText::SetText(const std::string &s)
+{
+	if (this->s != s) {
+		this->s = s;
+		free(ws);
+		ws = Str::Utf8ToWide(s.c_str());
+		wsLen = wcslen(ws);
+		Update();
+	}
+}
+
+void StaticText::Update()
+{
+	free(bitmap);
+
+	HDC hdc = CreateCompatibleDC(NULL);
+
+	HFONT stdFont = CreateFont(
+		size,
+		0, 0, 0,
+		bold ? FW_BOLD : FW_NORMAL,
+		italic ? TRUE : FALSE,
+		0, 0, 0, 0, 0, 0, 0,
+		font.c_str());
+	HFONT oldFont = (HFONT)SelectObject(hdc, stdFont);
+
+	// Get the dimensions required for the font.
+	RECT sz;
+	memset(&sz, 0, sizeof(sz));
+	DrawTextW(hdc, ws, wsLen, &sz, DT_CALCRECT | DT_NOPREFIX);
+	width = sz.right - sz.left;
+	height = sz.bottom - sz.top;
+
+	// Create a monochrome DIB to draw the text onto.
+	BITMAPINFO *bmpInfo =
+		(BITMAPINFO*)malloc(sizeof(BITMAPINFOHEADER) + 2 * sizeof(RGBQUAD));
+	BITMAPINFOHEADER &bmih = bmpInfo->bmiHeader;
+	memset(&bmih, 0, sizeof(bmih));
+	bmih.biSize = sizeof(bmih);
+	bmih.biWidth = width;
+	bmih.biHeight = -height;
+	bmih.biPlanes = 1;
+	bmih.biBitCount = 8;
+	bmih.biCompression = BI_RGB;
+	bmih.biClrUsed = 2;
+	bmih.biClrImportant = 2;
+	bmpInfo->bmiColors[0] = RGB_BLACK;
+	bmpInfo->bmiColors[1] = RGB_WHITE;
+
+	MR_UInt8 *bits;
+	HBITMAP bmp = CreateDIBSection(hdc, bmpInfo, DIB_RGB_COLORS,
+		(void**)&bits, NULL, 0);
+	HBITMAP oldBmp = (HBITMAP)SelectObject(hdc, bmp);
+
+	// Draw the text.
+	SetTextColor(hdc, RGB(0xff, 0xff, 0xff));
+	SetBkColor(hdc, RGB(0, 0, 0));
+	SetBkMode(hdc, OPAQUE);
+	DrawTextW(hdc, ws, wsLen, &sz, DT_NOCLIP | DT_NOPREFIX);
+
+	// Now copy from the bitmap into our image buffer.
+	// DIB rows are 32-bit word-aligned.
+	bitmap = (MR_UInt8*)malloc(width * height);
+	memset(bitmap, 0, width * height);
+	int padding = 4 - (width & 3);
+	if (padding == 4) padding = 0;
+	MR_UInt8 *src = bits;
+	MR_UInt8 *dest = bitmap;
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			*dest++ = (*src++ > 0) ? 0xff : 0x00;
+			//OutputDebugString(bitmap[y * width + x] ? "8" : "1");
+		}
+		src += padding;
+		//OutputDebugString("\n");
+	}
+
+	SelectObject(hdc, oldBmp);
+	DeleteObject(bmp);
+	free(bmpInfo);
+
+	SelectObject(hdc, oldFont);
+	DeleteObject(stdFont);
+
+	DeleteDC(hdc);
+}
+
+/**
+ * Draw the text onto the viewport.
+ * @param x The X coordinate (upper-left corner of text,
+ *          or middle of text if @e centerX is set).
+ * @param y The Y coordinate (upper-left corner of text).
+ * @param centerX Center the text on the X coordinate.
+ * @param vp The viewport (may not be @c NULL).
+ */
+void StaticText::Blt(int x, int y, MR_2DViewPort *vp, bool centerX)
+{
+	if (centerX) x -= width / 2;
+
+	int dw = vp->GetXRes();
+	int dh = vp->GetYRes();
+	int sx1, sy1, sx2, sy2;
+	int dx1, dy1;
+
+	// Check if the text is completely off the screen.
+	if (x >= dw || y >= dh || x + width <= 0 || y + height <= 0) return;
+
+	// Clip the text to the viewport.
+	if (x < 0) { sx1 = -x; dx1 = 0; } else { sx1 = 0; dx1 = x; }
+	if (y < 0) { sy1 = -y; dy1 = 0; } else { sy1 = 0; dy1 = y; }
+	sx2 = (width - sx1 + dx1 >= dw) ? (sx1 + dw - dx1) : width;
+	sy2 = (height - sy1 + dy1 >= dh) ? (sy1 + dh - dy1) : height;
+
+	int rowLen = vp->GetLineLen();
+	MR_UInt8 *destRow = vp->GetBuffer() + (rowLen * dy1 + dx1);
+	MR_UInt8 *dest = destRow;
+	for (int y = sy1; y < sy2; ++y) {
+		for (int x = sx1; x < sx2; ++x) {
+			if (bitmap[y * width + x] > 0) *dest = color;
+			++dest;
+		}
+		dest = (destRow += rowLen);
+	}
+}
+
---
+++ engine/VideoServices/StaticText.h
@@ -0,0 +1,84 @@
+
+// StaticText.h
+// Header for the StaticText renderable.
+//
+// Copyright (c) 2009 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#ifdef _WIN32
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+#include <string>
+
+#include "../Util/MR_Types.h"
+
+class MR_2DViewPort;
+
+namespace HoverRace {
+namespace VideoServices {
+
+/**
+ * An i18n-capable text renderable.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare StaticText {
+	public:
+		StaticText(const std::string &s="",
+			const std::string &font="Arial",
+			int size=20, bool bold=false, bool italic=false,
+			MR_UInt8 color=43);
+		virtual ~StaticText();
+
+		int GetWidth() const;
+		int GetHeight() const;
+
+		void SetText(const std::string &s);
+
+	private:
+		void Update();
+
+	public:
+		void Blt(int x, int y, MR_2DViewPort *pDest, bool centerX=false);
+
+	private:
+		std::string s;
+		wchar_t *ws;
+		size_t wsLen;
+
+		std::string font;
+		int size;
+		bool bold;
+		bool italic;
+		MR_UInt8 color;
+
+		MR_UInt8 *bitmap;
+		int width;
+		int height;
+};
+
+}  // namespace VideoServices
+}  // namespace HoverRace
