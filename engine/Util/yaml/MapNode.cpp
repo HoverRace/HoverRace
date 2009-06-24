ae69c3009e6fae16240f7e95af11dac143b6a438
Michael Imamura <zoogie@lugatgt.org>
2009-06-24 02:48:21 +0000

    Licensing for our LibYAML C++ wrapper.

--- engine/Util/yaml/MapNode.cpp
+++ engine/Util/yaml/MapNode.cpp
@@ -1,6 +1,24 @@
 
-/* yaml/MapNode.cpp
-	Wrapper for LibYAML mapping nodes. */
+// yaml/MapNode.cpp
+// Wrapper for LibYAML mapping nodes.
+//
+// Copyright (c) 2008, 2009 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
 
 #include "StdAfx.h"
 
