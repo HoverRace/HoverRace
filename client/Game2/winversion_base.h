203f18b8f3611fcdc92f8e51d3fffb867aa94b0f
Ryan Curtin <ryan@igglybob.com>
2009-09-19 21:15:38 +0000

    Add base file that SubWCRev.exe will modify.  Later this can be merged with the version.h that is used in the Linux build.

---
+++ client/Game2/winversion_base.h
@@ -0,0 +1,26 @@
+// winversion.h
+//
+// Automatically generated versioning information for Windows build, created
+// at build-time using TortoiseSVN's SubWCRev.exe utility
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
+#ifdef $WCMODS?0:1$
+	#define SVNREV $WCREV$
+#else
+	#define SVNREV 0 // if we have local modifications this cannot
+#endif // have an official revision number
