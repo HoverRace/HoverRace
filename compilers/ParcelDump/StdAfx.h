9f60cc4afe5750c99686fd6e2f81843dcfbff39c
Michael Imamura <zoogie@lugatgt.org>
2010-05-24 14:42:41 +0000

    #20: Headers needed to use engine in Linux.

--- compilers/ParcelDump/StdAfx.h
+++ compilers/ParcelDump/StdAfx.h
@@ -22,16 +22,35 @@
 #		include <afxcmn.h>						// MFC support for Windows 95 Common Controls
 #	endif										// _AFX_NO_AFXCMN_SUPPORT
 
+#	include "../../include/config-win32.h"
+
+#else
+
+#	include "../../include/compat/unix.h"
+#	include "../../config.h"
+
+#	include <string.h>
+#	include <strings.h>
+
 #endif
 
 #include <math.h>
 #include <stdio.h>
 
+#include <exception>
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
+#include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
+
+#ifndef _WIN32
+	// Xlib.h must be included *after* boost/foreach.hpp as a workaround for
+	// https://svn.boost.org/trac/boost/ticket/3000
+#	include <X11/Xlib.h>
+#endif
+
