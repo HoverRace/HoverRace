d36d1085ae8acde2269311d9378abcd65ab0cfe3
Michael Imamura <zoogie@lugatgt.org>
2010-09-23 05:08:34 +0000

    Added copyright and license header.

--- engine/VideoServices/Patch.h
+++ engine/VideoServices/Patch.h
@@ -1,8 +1,25 @@
 // Patch.h
 //
+// Copyright (c) 1995-1998 - Richard Langlois and Grokksoft Inc.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+//
 
-#ifndef MR_PATCH_H
-#define MR_PATCH_H
+#pragma once
 
 #include "../Util/WorldCoordinates.h"
 
@@ -34,4 +51,3 @@ class MR_DllDeclare MR_Patch
 };
 
 #undef MR_DllDeclare
-#endif
