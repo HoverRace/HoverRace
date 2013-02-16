1f2cd03d410978c1066751726a7b329b12d0e854
Michael Imamura <zoogie@lugatgt.org>
2013-02-16 12:32:31 -0500

    Move get_pointer() overloads to boost namespace.
    This mirrors how they're actually defined in Boost 1.53.

--- include/compat/boost_cxx11.h
+++ include/compat/boost_cxx11.h
@@ -8,12 +8,14 @@
 // These overloads are included in Boost 1.53.
 // See: https://svn.boost.org/trac/boost/ticket/4185
 #if BOOST_VERSION < 105300
-template<class T> T * get_pointer( std::unique_ptr<T> const& p )
-{
-    return p.get();
-}
-template<class T> T * get_pointer( std::shared_ptr<T> const& p )
-{
-    return p.get();
+namespace boost {
+	template<class T> T * get_pointer( std::unique_ptr<T> const& p )
+	{
+		return p.get();
+	}
+	template<class T> T * get_pointer( std::shared_ptr<T> const& p )
+	{
+		return p.get();
+	}
 }
 #endif
