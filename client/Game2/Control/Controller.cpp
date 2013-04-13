7bb0e4251b78d55f33f0b07de42d2c6b14dad850
Michael Imamura <zoogie@lugatgt.org>
2013-04-13 19:05:08 -0400

    Add copyright and license notice.

--- client/Game2/Control/Controller.cpp
+++ client/Game2/Control/Controller.cpp
@@ -1,8 +1,24 @@
+
 // Controller.cpp
 //
-// This file contains the definition for the HoverRace::Client::Controller
-// class.  It contains all the code to control input devices; joysticks, 
-// keyboards, and the like.
+// Copyright (c) 2010 Ryan Curtin.
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
 
 #include "StdAfx.h"
 
