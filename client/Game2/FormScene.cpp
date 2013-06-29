2f70b58e9cbfbdc92257eda905d2eb9f571672af
Michael Imamura <zoogie@lugatgt.org>
2013-06-28 20:27:45 -0400

    Add initial skeleton for FormScene.

---
+++ client/Game2/FormScene.cpp
@@ -0,0 +1,73 @@
+
+// FormScene.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../../engine/Control/Controller.h"
+
+#include "FormScene.h"
+
+namespace HoverRace {
+namespace Client {
+
+FormScene::FormScene(Display::Display &display, const std::string &name) :
+	SUPER(name), display(display)
+{
+}
+
+FormScene::~FormScene()
+{
+}
+
+void FormScene::AttachController(Control::InputEventController &controller)
+{
+	mouseMovedConn = controller.actions.ui.mouseMoved->Connect(
+		std::bind(&FormScene::OnMouseMoved, this, std::placeholders::_1));
+	mousePressedConn = controller.actions.ui.mousePressed->Connect(
+		std::bind(&FormScene::OnMousePressed, this, std::placeholders::_1));
+	mouseReleasedConn = controller.actions.ui.mouseReleased->Connect(
+		std::bind(&FormScene::OnMouseReleased, this, std::placeholders::_1));
+}
+
+void FormScene::DetachController(Control::InputEventController &controller)
+{
+	mouseReleasedConn.disconnect();
+	mousePressedConn.disconnect();
+	mouseMovedConn.disconnect();
+}
+
+void FormScene::OnMouseMoved(const Display::Vec2 &pos)
+{
+	//TODO: Convert to UI coords and pass to root container.
+}
+
+void FormScene::OnMousePressed(const Control::Mouse::Click &pos)
+{
+	//TODO: Convert to UI coords and pass to root container.
+}
+
+void FormScene::OnMouseReleased(const Control::Mouse::Click &pos)
+{
+	//TODO: Convert to UI coords and pass to root container.
+}
+
+}  // namespace Client
+}  // namespace HoverRace
---
+++ client/Game2/FormScene.h
@@ -0,0 +1,64 @@
+
+// FormScene.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "UiScene.h"
+
+namespace HoverRace {
+	namespace Display {
+		class Display;
+	}
+}
+
+namespace HoverRace {
+namespace Client {
+
+/**
+ * Base class for full-scene menus and the like.
+ * @author Michael Imamura
+ */
+class FormScene : public UiScene
+{
+	typedef UiScene SUPER;
+	public:
+		FormScene(Display::Display &display, const std::string &name="");
+		virtual ~FormScene();
+
+	public:
+		virtual void AttachController(Control::InputEventController &controller);
+		virtual void DetachController(Control::InputEventController &controller);
+
+	private:
+		void OnMouseMoved(const Display::Vec2 &pos);
+		void OnMousePressed(const Control::Mouse::Click &pos);
+		void OnMouseReleased(const Control::Mouse::Click &pos);
+
+	protected:
+		Display::Display &display;
+	private:
+		boost::signals2::connection mouseMovedConn;
+		boost::signals2::connection mousePressedConn;
+		boost::signals2::connection mouseReleasedConn;
+};
+
+}  // namespace Client
+}  // namespace HoverRace
