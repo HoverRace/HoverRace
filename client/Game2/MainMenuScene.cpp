7fedc8ba301e990f88bdfb605b3632807ca6d8aa
Michael Imamura <zoogie@lugatgt.org>
2013-07-06 14:45:51 -0400

    Placeholder for main menu scene.

---
+++ client/Game2/MainMenuScene.cpp
@@ -0,0 +1,50 @@
+
+// MainMenuScene.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../../engine/Control/Controller.h"
+#include "../../engine/Display/Container.h"
+#include "../../engine/Display/Display.h"
+#include "../../engine/Display/FillBox.h"
+#include "../../engine/Display/Label.h"
+#include "../../engine/Display/ScreenFade.h"
+#include "../../engine/VideoServices/FontSpec.h"
+#include "../../engine/Util/Config.h"
+
+#include "MainMenuScene.h"
+
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Client {
+
+MainMenuScene::MainMenuScene(Display::Display &display) :
+	SUPER(display, "Main Menu")
+{
+}
+
+MainMenuScene::~MainMenuScene()
+{
+}
+
+}  // namespace Client
+}  // namespace HoverRace
