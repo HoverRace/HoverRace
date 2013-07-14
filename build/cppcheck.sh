2d002217b47a5a09b64bdcc904ce284a21fedaca
Michael Imamura <zoogie@lugatgt.org>
2013-07-14 13:42:39 -0400

    Add script to run cppcheck against our sources.

---
+++ build/cppcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cppcheck
+#   Runs cppcheck, a static code analyzer, against all C++ sources.
+#   This assumes that cppcheck is available in the path.
+#   http://cppcheck.sourceforge.net/
+#
+# After checking, the report of warnings is available in: cppcheck_report.txt
+
+CPPCHECK=cppcheck
+
+cd "$(dirname "$0")/.."
+
+( find client/Game2 -name '*.cpp' ; find engine -name '*.cpp' ) | \
+	"$CPPCHECK" --file-list=- 2> >(tee cppcheck_report.txt >&2)
+
