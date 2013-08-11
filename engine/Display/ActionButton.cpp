2b86363b84a78be43ad1d3f8905e6fcd67a073ed
Michael Imamura <zoogie@lugatgt.org>
2013-08-10 20:43:14 -0400

    Add ActionButton.

---
+++ engine/Display/ActionButton.cpp
@@ -0,0 +1,96 @@
+
+// ActionButton.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "ActionButton.h"
+
+namespace HoverRace {
+namespace Display {
+
+/**
+ * Constructor for automatically-sized button.
+ * @note The button will have no text until an action is attached.
+ * @param display The display child elements will be attached to.
+ * @param layoutFlags Optional layout flags.
+ */
+ActionButton::ActionButton(Display &display, uiLayoutFlags_t layoutFlags) :
+	SUPER(display, "", layoutFlags)
+{
+}
+
+/**
+ * Constructor for fixed-sized button.
+ * @note The button will have no text until an action is attached.
+ * @param display The display child elements will be attached to.
+ * @param size The fixed button size.
+ * @param layoutFlags Optional layout flags.
+ */
+ActionButton::ActionButton(Display &display, const Vec2 &size,
+                           uiLayoutFlags_t layoutFlags) :
+	SUPER(display, size, "", layoutFlags)
+{
+}
+
+ActionButton::~ActionButton()
+{
+}
+
+/**
+ * Attach an action to the button.
+ * This updates the button's text with the name and hotkey of the action.
+ * Clicking on the button will fire the attached action.
+ * This is typically called from Client::Scene::AttachController.
+ * @param controller The controller that owns the action.
+ * @param action The action to attach.
+ */
+void ActionButton::AttachAction(Control::InputEventController &controller,
+	Control::InputEventController::VoidActionPtr action)
+{
+	this->action = action;
+
+	std::ostringstream oss;
+	oss << '[' << controller.HashToString(action->GetPrimaryTrigger()) <<
+		"] " << action->GetName();
+	SetText(oss.str());
+}
+
+/**
+ * Detach the currently-attached action (if any).
+ * @note It is usually unnecessary to call this from
+ *       Client::Scene::DetachController since this class maintains a weak
+ *       reference to the attached action.
+ */
+void ActionButton::DetachAction()
+{
+	action.reset();
+}
+
+void ActionButton::FireClickedSignal()
+{
+	SUPER::FireClickedSignal();
+	if (auto actionLock = action.lock()) {
+		(*actionLock)(1);
+	}
+}
+
+}  // namespace Display
+}  // namespace HoverRace
---
+++ engine/Display/ActionButton.h
@@ -0,0 +1,76 @@
+
+// ActionButton.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "../Control/Controller.h"
+
+#include "Button.h"
+
+#ifdef _WIN32
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+namespace Display {
+
+/**
+ * A button that's bound to a controller action.
+ *
+ * This button gets its text from the hotkey and label of the attached action.
+ *
+ * When the button is clicked, then it fires both the "clicked" signal as well
+ * as the attached action.  That way, if the scene is already listening to the
+ * action, it doesn't need to explicitly set the clicked signal on the button.
+ *
+ * @author Michael Imamura
+ */
+class MR_DllDeclare ActionButton : public Button
+{
+	typedef Button SUPER;
+	public:
+		ActionButton(Display &display, uiLayoutFlags_t layoutFlags=0);
+		ActionButton(Display &display, const Vec2 &size,
+			uiLayoutFlags_t layoutFlags=0);
+		virtual ~ActionButton();
+
+	public:
+		void AttachAction(Control::InputEventController &controller,
+			Control::InputEventController::VoidActionPtr action);
+		void DetachAction();
+
+	protected:
+		virtual void FireClickedSignal();
+
+	private:
+		std::weak_ptr<Control::InputEventController::VoidActionPtr::element_type> action;
+};
+
+}  // namespace Display
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
