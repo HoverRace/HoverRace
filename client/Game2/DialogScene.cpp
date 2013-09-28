fc0e395e1cfd1a54e709db9f1e2a8f15267f2541
Michael Imamura <zoogie@lugatgt.org>
2013-09-27 21:48:29 -0400

    Add DialogScene.

---
+++ client/Game2/DialogScene.cpp
@@ -0,0 +1,77 @@
+
+// FormScene.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../../engine/Display/Container.h"
+#include "../../engine/Display/Label.h"
+#include "../../engine/Util/Config.h"
+#include "../../engine/Util/Log.h"
+
+#include "DialogScene.h"
+
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Client {
+
+const double DialogScene::MARGIN_WIDTH = 40;
+
+/**
+ * Constructor.
+ * @param display The display.
+ * @param title The title to display.
+ * @param name The name of the scene.  See Scene::GetName.
+ */
+DialogScene::DialogScene(Display::Display &display,
+                         const std::string &title,
+                         const std::string &name) :
+	SUPER(display, name),
+	title(title)
+{
+	typedef Display::UiViewModel::Alignment Alignment;
+
+	Config *cfg = Config::GetInstance();
+	const std::string &fontName = cfg->GetDefaultFontName();
+
+	auto root = GetRoot();
+
+	contentRoot = root->AddChild(new Display::Container(
+		display, Display::Vec2(1280, 720 - 200)));
+	contentRoot->SetPos(0, 100);
+
+	statusRoot = root->AddChild(new Display::Container(
+		display, Display::Vec2(1280 - (MARGIN_WIDTH * 2), 40)));
+	statusRoot->SetPos(MARGIN_WIDTH, 720 - 80);
+
+	auto titleLbl = root->AddChild(new Display::Label(title,
+		Display::UiFont(fontName, 40, Display::UiFont::BOLD),
+		Display::COLOR_WHITE));
+	titleLbl->SetPos(MARGIN_WIDTH, 80);
+	titleLbl->SetAlignment(Alignment::SW);
+}
+
+DialogScene::~DialogScene()
+{
+}
+
+}  // namespace Client
+}  // namespace HoverRace
---
+++ client/Game2/DialogScene.h
@@ -0,0 +1,54 @@
+
+// DialogScene.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "FormScene.h"
+
+namespace HoverRace {
+namespace Client {
+
+/**
+ * Base class for scenes with a title and status area.
+ * @author Michael Imamura
+ */
+class DialogScene : public FormScene
+{
+	typedef FormScene SUPER;
+	public:
+		DialogScene(Display::Display &display, const std::string &title="",
+			const std::string &name="");
+		virtual ~DialogScene();
+
+	protected:
+		Display::Container *GetContentRoot() const { return contentRoot.get(); }
+		Display::Container *GetStatusRoot() const { return statusRoot.get(); }
+
+	public:
+		static const double MARGIN_WIDTH;
+	private:
+		std::string title;
+		std::shared_ptr<Display::Container> contentRoot;
+		std::shared_ptr<Display::Container> statusRoot;
+};
+
+}  // namespace Client
+}  // namespace HoverRace
