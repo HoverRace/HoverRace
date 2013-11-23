2b8d45bd882980d6f32690a992a9a7f6cb935023
Michael Imamura <zoogie@lugatgt.org>
2013-11-22 23:21:04 -0500

    Add basic wrapper for SDL_Texture.

---
+++ engine/Display/SDL/SdlTexture.h
@@ -0,0 +1,76 @@
+
+// SdlTexture.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include "../Texture.h"
+
+#ifdef _WIN32
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+	namespace Display {
+		class SdlDisplay;
+	}
+}
+
+namespace HoverRace {
+namespace Display {
+namespace SDL {
+
+/**
+ * Wrapper for SDL_Texture.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare SdlTexture : public Texture, private boost::noncopyable
+{
+	typedef Texture SUPER;
+	public:
+		SdlTexture(SdlDisplay &display, SDL_Texture *texture) :
+			SUPER(), texture(texture), display(display) { }
+
+		virtual ~SdlTexture()
+		{
+			if (texture) SDL_DestroyTexture(texture);
+		}
+
+	public:
+		SDL_Texture *Get() const { return texture; }
+
+	protected:
+		SDL_Texture *texture;
+		SdlDisplay &display;
+};
+
+}  // namespace SDL
+}  // namespace Display
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
