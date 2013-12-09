860295e1e5744a0d41543d1ed7b7f2418f58d7d3
Michael Imamura <zoogie@lugatgt.org>
2013-12-08 21:32:37 -0500

    Add initial Clock.

---
+++ engine/Util/Clock.cpp
@@ -0,0 +1,46 @@
+
+// Clock.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "Clock.h"
+
+namespace HoverRace {
+namespace Util {
+
+/**
+ * Constructor.
+ * @param init The initial simulation time.
+ */
+Clock::Clock(OS::timestamp_t init) :
+	start(OS::Time()),
+	offset(OS::TimeDiff(start, init))
+{
+}
+
+void Clock::SetTime(OS::timestamp_t ts)
+{
+	start = OS::Time();
+	offset = OS::TimeDiff(start, ts);
+}
+
+}  // namespace Util
+}  // namespace HoverRace
---
+++ engine/Util/Clock.h
@@ -0,0 +1,63 @@
+
+// Clock.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "OS.h"
+
+#ifdef _WIN32
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+namespace Util {
+
+/**
+ * A game clock that manages the current time in the simulation.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare Clock
+{
+	public:
+		Clock(OS::timestamp_t init=0);
+
+	public:
+		OS::timestamp_t GetTime() const {
+			return OS::TimeDiff(OS::Time(), offset);
+		}
+
+		void SetTime(OS::timestamp_t ts=0);
+
+	private:
+		OS::timestamp_t start;
+		OS::timestamp_t offset;
+};
+
+}  // namespace Util
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
