f2f226aad2499ce0282b28ca4a487bc773354b4d
Michael Imamura <zoogie@lugatgt.org>
2013-12-13 20:49:49 -0500

    Add basic stopwatch.

---
+++ engine/Util/Stopwatch.cpp
@@ -0,0 +1,67 @@
+
+// Stopwatch.cpp
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "Stopwatch.h"
+
+namespace HoverRace {
+namespace Util {
+
+/**
+ * Constructor.
+ * The start time of the first lap is recorded as the current time of the
+ * clock.
+ * @param clock The clock to use for timing.
+ */
+Stopwatch::Stopwatch(std::shared_ptr<Clock> clock) :
+	clock(std::move(clock)), lastLap(clock->GetTime())
+{
+}
+
+/**
+ * Constructor.
+ * @param clock The clock to use for timing.
+ * @param start The timestamp of the start of the first lap.
+ */
+Stopwatch::Stopwatch(std::shared_ptr<Clock> clock, OS::timestamp_t start) :
+	clock(std::move(clock)), lastLap(start)
+{
+}
+
+/**
+ * Start a new lap.
+ * @param name The name of the lap that just ended.
+ */
+OS::timestamp_t Stopwatch::NextLap(const std::string &name)
+{
+	OS::timestamp_t ts = clock->GetTime();
+	OS::timestamp_t elapsed = OS::TimeDiff(ts, lastLap);
+	lastLap = ts;
+
+	laps.emplace_back(Lap(name, elapsed));
+
+	return elapsed;
+}
+
+}  // namespace Util
+}  // namespace HoverRace
+
---
+++ engine/Util/Stopwatch.h
@@ -0,0 +1,79 @@
+
+// Stopwatch.h
+//
+// Copyright (c) 2013 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "Clock.h"
+#include "OS.h"
+
+#ifdef _WIN32
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+namespace Util {
+
+/**
+ * Records lap times.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare Stopwatch
+{
+	public:
+		struct Lap
+		{
+			Lap(const std::string &name, OS::timestamp_t elapsed) :
+				name(name), elapsed(elapsed) { }
+
+			const std::string name;
+			const OS::timestamp_t elapsed;
+		};
+
+	public:
+		Stopwatch(std::shared_ptr<Clock> clock);
+		Stopwatch(std::shared_ptr<Clock> clock, OS::timestamp_t start);
+
+	public:
+		template<typename Fn>
+		void ForEachLap(Fn fn)
+		{
+			std::for_each(laps.begin(), laps.end(), fn);
+		}
+
+	public:
+		OS::timestamp_t NextLap(const std::string &name);
+
+	private:
+		std::shared_ptr<Clock> clock;
+		OS::timestamp_t lastLap;
+		std::vector<Lap> laps;
+};
+
+}  // namespace Util
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
