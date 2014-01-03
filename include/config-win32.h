0b2eb4426efcb88773e017608c472a3be163eb30
Michael Imamura <zoogie@lugatgt.org>
2014-01-02 20:25:30 -0500

    Remove unused WITH_SDL define.

--- include/config-win32.h
+++ include/config-win32.h
@@ -29,8 +29,5 @@
 
 #define WITH_OBJSTREAM 1
 
-// Uncomment this to use the SDL renderer and client.
-#define WITH_SDL 1
-
 // Uncomment this to use SDL_Pango to render text.
 //#define WITH_SDL_PANGO
