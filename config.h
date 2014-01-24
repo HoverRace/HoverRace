785518ecf1f2dd1a1f5f4a99f053f36f551c8362
Michael Imamura <zoogie@lugatgt.org>
2014-01-24 00:03:56 -0500

    Add temporary config.h and config.h.in.
    This is a temporary fix for the missing config.h when using a pure-CMake
    build.
    
    Issue #238.

---
+++ config.h
@@ -0,0 +1,45 @@
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#define ENABLE_NLS 1
+
+/* Define to 1 if you have the declaration of `strerror_r', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRERROR_R 1
+
+/* Define if you have the iconv() function and it works. */
+#define HAVE_ICONV 1
+
+/* Define to 1 if you have the `strerror_r' function. */
+#define HAVE_STRERROR_R 1
+
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST
+
+/* Name of package */
+#define PACKAGE "hoverrace"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "HoverRace"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "HoverRace 2.0"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.0"
+
+/* Platform name. */
+#define PLATFORM_NAME "Linux"
+
+/* Define to 1 if strerror_r returns char *. */
+#define STRERROR_R_CHAR_P 1
+
+/* Version number of package */
+#define VERSION "2.0"
+
+/* Using OpenAL */
+#define WITH_OPENAL 1
+
+/* Using SDL2_ttf */
+#define WITH_SDL_TTF 1
+
