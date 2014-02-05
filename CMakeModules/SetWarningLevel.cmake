584ab48ab04285ca6756f37715438db7445c392e
Michael Imamura <zoogie@lugatgt.org>
2014-02-04 23:57:19 -0500

    Allow scope to be specified in SetWarningLevel.

--- CMakeModules/SetWarningLevel.cmake
+++ CMakeModules/SetWarningLevel.cmake
@@ -1,18 +1,18 @@
 
-function(set_warning_level_direct target gcc_opt msvc_opt)
+function(set_warning_level_direct scope gcc_opt msvc_opt)
 	if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 		set(opt "${gcc_opt}")
 	elseif(MSVC)
 		set(opt "${msvc_opt}")
 	endif()
-	set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS ${opt})
-endfunction(set_warning_level_direct)
+	set_property(${scope} APPEND_STRING PROPERTY COMPILE_FLAGS " ${opt} ")
+endfunction()
 
-function(set_full_warnings target)
-	set_warning_level_direct(${target} "-Winvalid-pch -Wall" "/W3")
-endfunction(set_full_warnings)
+function(set_full_warnings)
+	set_warning_level_direct("${ARGN}" "-Winvalid-pch -Wall" "/W3")
+endfunction()
 
-function(set_no_warnings target)
-	set_warning_level_direct(${target} "-w" "/w")
-endfunction(set_no_warnings)
+function(set_no_warnings)
+	set_warning_level_direct("${ARGN}" "-w" "/w")
+endfunction()
 
