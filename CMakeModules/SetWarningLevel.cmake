f15534533a409d2eea0482a6e3c051aa05506bd7
Michael Imamura <zoogie@lugatgt.org>
2014-02-04 23:21:37 -0500

    Simplify and use functions.

--- CMakeModules/SetWarningLevel.cmake
+++ CMakeModules/SetWarningLevel.cmake
@@ -1,21 +1,18 @@
 
-macro(set_warning_level_direct target val)
-	set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS ${val})
-endmacro(set_warning_level_direct)
-
-macro(set_full_warnings target)
+function(set_warning_level_direct target gcc_opt msvc_opt)
 	if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-		set_warning_level_direct(${target} "-Winvalid-pch -Wall")
+		set(opt "${gcc_opt}")
 	elseif(MSVC)
-		set_warning_level_direct(${target} "/W3")
+		set(opt "${msvc_opt}")
 	endif()
-endmacro(set_full_warnings)
+	set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS ${opt})
+endfunction(set_warning_level_direct)
 
-macro(set_no_warnings target)
-	if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-		set_warning_level_direct(${target} "-w")
-	elseif(MSVC)
-		set_warning_level_direct(${target} "/W0")
-	endif()
-endmacro(set_no_warnings)
+function(set_full_warnings target)
+	set_warning_level_direct(${target} "-Winvalid-pch -Wall" "/W3")
+endfunction(set_full_warnings)
+
+function(set_no_warnings target)
+	set_warning_level_direct(${target} "-w" "/w")
+endfunction(set_no_warnings)
 
