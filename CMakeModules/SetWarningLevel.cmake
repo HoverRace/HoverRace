7ea69c80c48bce67e5b8ede05ff26740fb585feb
Michael Imamura <zoogie@lugatgt.org>
2014-02-04 21:51:28 -0500

    Set warnings on a per-target basis.

--- CMakeModules/SetWarningLevel.cmake
+++ CMakeModules/SetWarningLevel.cmake
@@ -1,8 +1,21 @@
 
-# Bump the warning level.
-if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Winvalid-pch -Wall")
-elseif(MSVC)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3")
-endif()
+macro(set_warning_level_direct target val)
+	set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS ${val})
+endmacro(set_warning_level_direct)
+
+macro(set_full_warnings target)
+	if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		set_warning_level_direct(${target} "-Winvalid-pch -Wall")
+	elseif(MSVC)
+		set_warning_level_direct(${target} "/W3")
+	endif()
+endmacro(set_full_warnings)
+
+macro(set_no_warnings target)
+	if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		set_warning_level_direct(${target} "-w")
+	elseif(MSVC)
+		set_warning_level_direct(${target} "/W0")
+	endif()
+endmacro(set_no_warnings)
 
