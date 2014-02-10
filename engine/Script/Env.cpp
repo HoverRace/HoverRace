a0d8f379ac378f93fc3e6ca1498db934c9282c3d
Michael Imamura <zoogie@lugatgt.org>
2014-02-10 17:29:11 -0500

    Update class doc and copyright.

--- engine/Script/Env.cpp
+++ engine/Script/Env.cpp
@@ -1,8 +1,7 @@
 
 // Env.cpp
-// An environment in the scripting engine.
 //
-// Copyright (c) 2010 Michael Imamura.
+// Copyright (c) 2010, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
