3d88becbf969dabe098d84b0b4a4dd49d47a7216
Michael Imamura <zoogie@lugatgt.org>
2014-03-04 19:30:54 -0500

    Update copyright and file heading.

--- client/Game2/GameDirector.h
+++ client/Game2/GameDirector.h
@@ -1,8 +1,7 @@
 
 // GameDirector.h
-// Interface for game client shells.
 //
-// Copyright (c) 2010, 2013 Michael Imamura.
+// Copyright (c) 2010, 2013, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
--- engine/Util/Str.cpp
+++ engine/Util/Str.cpp
@@ -1,8 +1,7 @@
 
-// Str.h
-// String support functions.
+// Str.cpp
 //
-// Copyright (c) 2009 Michael Imamura.
+// Copyright (c) 2009, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
