32bace9ec3648a189a57a30e8a0f77f3252dfbb9
Michael Imamura <zoogie@lugatgt.org>
2014-03-09 18:31:10 -0400

    Generalize wrapper script copying into module.

---
+++ CMakeModules/CopyWrapperScript.cmake
@@ -0,0 +1,11 @@
+
+function(copy_wrapper_script src dest)
+	configure_file(${src} ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${dest})
+	file(COPY ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${dest}
+		DESTINATION ${CMAKE_BINARY_DIR}
+		FILE_PERMISSIONS
+			OWNER_READ OWNER_WRITE OWNER_EXECUTE
+			GROUP_READ GROUP_WRITE GROUP_EXECUTE
+			WORLD_READ             WORLD_EXECUTE)
+endfunction()
+
