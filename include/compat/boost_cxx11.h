695294f1fdd973adc742ac0f2f141533f452739a
Michael Imamura <zoogie@lugatgt.org>
2014-04-20 22:12:10 -0400

    Use get_pointer fix in newer Boost if needed.
    Clang 3.3 and Boost 1.54 (via Ubuntu 14.04 LTS) doesn't set up these
    defines for some reason.  We can detect it via BOOST_NO_CXX11_SMART_PTR.
    
    Fixes #256.

--- include/compat/boost_cxx11.h
+++ include/compat/boost_cxx11.h
@@ -1,13 +1,14 @@
 
 #pragma once
 
+#include <boost/config.hpp>
 #include <boost/version.hpp>
 #include <memory>
 
 // Support for C++11 smart pointers.
 // These overloads are included in Boost 1.53.
 // See: https://svn.boost.org/trac/boost/ticket/4185
-#if BOOST_VERSION < 105300
+#if BOOST_VERSION < 105300 || defined(BOOST_NO_CXX11_SMART_PTR)
 namespace boost {
 	template<class T> T * get_pointer( std::unique_ptr<T> const& p )
 	{
