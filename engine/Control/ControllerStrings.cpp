92cced367d60c0b14fcdd2a35082da3a541d3dc0
Michael Imamura <zoogie@lugatgt.org>
2014-06-02 20:10:14 -0400

    Update copyright.

--- engine/Control/ControllerStrings.cpp
+++ engine/Control/ControllerStrings.cpp
@@ -2,7 +2,7 @@
 // ControllerStrings.cpp
 //
 // Copyright (c) 2010 Ryan Curtin.
-// Copyright (c) 2013 Michael Imamura.
+// Copyright (c) 2013, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
