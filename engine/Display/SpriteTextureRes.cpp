d03c555964c07fb14e83eebb8ad5c669d4a59a60
Michael Imamura <zoogie@lugatgt.org>
2014-07-28 00:14:07 -0400

    Add SpriteTextureRes.

---
+++ engine/Display/SpriteTextureRes.cpp
@@ -0,0 +1,75 @@
+
+// SpriteTextureRes.cpp
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../engine/Display/Texture.h"
+#include "../engine/Parcel/ObjStream.h"
+
+#include "SpriteTextureRes.h"
+
+namespace HoverRace {
+namespace Display {
+
+/**
+ * Constructor.
+ * @param recordName The name of the record being loaded.
+ * @param archive The input object stream.
+ */
+SpriteTextureRes::SpriteTextureRes(const std::string &recordName,
+	Parcel::ObjStream &archive) :
+	SUPER(),
+	id("spriteTexture:" + recordName)
+{
+	imageData.pixels = nullptr;
+
+	MR_UInt32 numItems, itemHeight, totalHeight, width;
+
+	archive >> numItems >> itemHeight >> totalHeight >> width;
+
+	imageData.pixels = malloc(width * totalHeight);
+	imageData.width = width;
+	imageData.height = totalHeight;
+	imageData.depth = 8;
+	imageData.pitch = width;
+	imageData.rMask = 0;
+	imageData.gMask = 0;
+	imageData.bMask = 0;
+	imageData.aMask = 0;
+
+	try {
+		archive.Read(imageData.pixels, width * totalHeight);
+	}
+	catch (Parcel::ObjStreamExn&) {
+		free(imageData.pixels);
+		throw;
+	}
+}
+
+SpriteTextureRes::~SpriteTextureRes()
+{
+	if (imageData.pixels) {
+		free(imageData.pixels);
+	}
+}
+
+}  // namespace Display
+}  // namespace HoverRace
---
+++ engine/Display/SpriteTextureRes.h
@@ -0,0 +1,80 @@
+
+// SpriteTextureRes.h
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "Res.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+	namespace Display {
+		class Texture;
+	}
+	namespace Parcel {
+		class ObjStream;
+	}
+}
+
+namespace HoverRace {
+namespace Display {
+
+/**
+ * Loads a legacy 8-bit sprite as a texture.
+ * @author Michael Imamura
+ */
+class SpriteTextureRes : public Res<Texture>
+{
+	typedef Res<Texture> SUPER;
+
+	public:
+		SpriteTextureRes(const std::string &recordName,
+			Parcel::ObjStream &archive);
+		virtual ~SpriteTextureRes();
+
+	public:
+		std::string GetId() const override { return id; }
+
+		std::unique_ptr<std::istream> Open() const override
+		{
+			throw ResLoadExn("Attempted to open internal texture.");
+		}
+
+		bool IsGenerated() const override { return true; }
+		const ImageData *GetImageData() override { return &imageData; }
+
+	private:
+		std::string id;
+		ImageData imageData;
+};
+
+}  // namespace Display
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
