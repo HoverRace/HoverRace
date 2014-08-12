e25568786e00e83c27f2874525bedf20abfeda64
Michael Imamura <zoogie@lugatgt.org>
2014-08-12 19:27:52 -0400

    Add initial loading scene.

---
+++ client/Game2/LoadingScene.cpp
@@ -0,0 +1,100 @@
+
+// LoadingScene.cpp
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "StdAfx.h"
+
+#include "../../engine/Display/ScreenFade.h"
+#include "../../engine/Util/Log.h"
+
+#include "LoadingScene.h"
+
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Client {
+
+LoadingScene::LoadingScene(Display::Display &display, GameDirector &director,
+                           const std::string &name) :
+	SUPER(display, name),
+	director(director), loading(false)
+{
+	using namespace Display;
+
+	SetPhaseTransitionDuration(1000);
+
+	const auto &s = display.styles;
+
+	fader.reset(new ScreenFade(s.dialogBg, 0.0));
+	fader->AttachView(display);
+}
+
+LoadingScene::~LoadingScene()
+{
+}
+
+void LoadingScene::OnPhaseChanged(Phase oldPhase)
+{
+	if (GetPhase() == Phase::RUNNING) {
+		loading = true;
+	}
+
+	SUPER::OnPhaseChanged(oldPhase);
+}
+
+void LoadingScene::OnPhaseTransition(double progress)
+{
+	if (fader) {
+		fader->SetOpacity(progress);
+	}
+
+	SUPER::OnPhaseTransition(progress);
+}
+
+void LoadingScene::PrepareRender()
+{
+	if (loading) {
+		//TODO: Load the next loadable.
+		//TODO: Only finish when actually finished.
+		if (true) {
+			loading = false;
+			finishedLoadingSignal();
+			director.RequestPopScene();
+		}
+	}
+
+	if (fader) {
+		fader->PrepareRender();
+	}
+
+	SUPER::PrepareRender();
+}
+
+void LoadingScene::Render()
+{
+	if (fader) {
+		fader->Render();
+	}
+
+	SUPER::Render();
+}
+
+}  // namespace Client
+}  // namespace HoverRace
---
+++ client/Game2/LoadingScene.h
@@ -0,0 +1,82 @@
+
+// LoadingScene.h
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "GameDirector.h"
+
+#include "FormScene.h"
+
+namespace HoverRace {
+	namespace Display {
+		class ScreenFade;
+	}
+}
+
+namespace HoverRace {
+namespace Client {
+
+/**
+ * Base class for "Please Wait" loading scenes.
+ *
+ * These scenes provide a way to wait for the previous scene stack to finish
+ * unloading resources then allow the new scene to load.
+ *
+ * @author Michael Imamura
+ */
+class LoadingScene : public FormScene
+{
+	typedef FormScene SUPER;
+	public:
+		LoadingScene(Display::Display &display, GameDirector &director,
+			const std::string &name="");
+		virtual ~LoadingScene();
+
+	public:
+		typedef boost::signals2::signal<void()> finishedLoadingSignal_t;
+		/**
+		 * Fired when all resources have been loaded and the loading scene
+		 * is shutting down.
+		 * @return The finished loading signal.
+		 */ 
+		finishedLoadingSignal_t &GetFinishedLoadingSignal()
+		{
+			return finishedLoadingSignal;
+		}
+
+	protected:
+		void OnPhaseChanged(Phase oldPhase) override;
+
+	public:
+		void OnPhaseTransition(double progress) override;
+		void PrepareRender() override;
+		void Render() override;
+
+	private:
+		GameDirector &director;
+		bool loading;
+		std::unique_ptr<Display::ScreenFade> fader;
+
+		finishedLoadingSignal_t finishedLoadingSignal;
+};
+
+}  // namespace Client
+}  // namespace HoverRace
