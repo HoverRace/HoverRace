ff214ade222f54aeb81ed92d97b3f47abda1eff0
Michael Imamura <zoogie@lugatgt.org>
2014-08-18 22:22:01 -0400

    Update copyright.

--- client/Game2/GameScene.h
+++ client/Game2/GameScene.h
@@ -1,7 +1,7 @@
 
 // GameScene.h
 //
-// Copyright (c) 2010, 2013 Michael Imamura.
+// Copyright (c) 2010, 2013, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
