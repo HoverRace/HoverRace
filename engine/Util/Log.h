87fb2876be9b2ae81164f99f8ee7bafad2a41a9c
Michael Imamura <zoogie@lugatgt.org>
2014-08-23 19:18:07 -0400

    Initial Boost.Log with varargs.

--- engine/Util/Log.h
+++ engine/Util/Log.h
@@ -21,6 +21,8 @@
 
 #pragma once
 
+#include <boost/log/trivial.hpp>
+
 #if defined(_WIN32) && defined(HR_ENGINE_SHARED)
 #	ifdef MR_ENGINE
 #		define MR_DllDeclare   __declspec( dllexport )
@@ -41,6 +43,24 @@ namespace Util {
 
 namespace Log {
 
+	namespace detail {
+		const size_t MAX_LOG = 512;
+
+		inline std::string Fmt(const char *fmt, va_list ap)
+		{
+			char buf[MAX_LOG];
+			memset(buf, 0, sizeof(buf));
+			if (vsnprintf(buf, sizeof(buf) - 1, fmt, ap) == -1) {
+				std::string retv{buf};
+				retv += "...<output truncated>";
+				return retv;
+			}
+			else {
+				return std::string(buf);
+			}
+		}
+	}
+
 	MR_DllDeclare void Init();
 
 	enum class Level {
@@ -59,11 +79,21 @@ namespace Log {
 	typedef boost::signals2::signal<void(const Entry&)> logAdded_t;
 	MR_DllDeclare extern logAdded_t logAddedSignal;
 
+	inline void Debug(const char *fmt, ...)
+	{
+		va_list ap;
+		va_start(ap, fmt);
+		BOOST_LOG_TRIVIAL(debug) << detail::Fmt(fmt, ap);
+		va_end(ap);
+	}
+
+#if 0
 #ifdef _DEBUG
 	MR_DllDeclare void Debug(const char *fmt, ...);
 #else
 	// Not a great solution (params are still evaluated), but suffices for now.
 	MR_DllDeclare inline void Debug(...) { }
+#endif
 #endif
 	MR_DllDeclare void Info(const char *fmt, ...);
 	MR_DllDeclare void Warn(const char *fmt, ...);
