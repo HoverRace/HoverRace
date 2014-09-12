6785da1aa591ba0c49fd028eba62694333b31e69
Michael Imamura <zoogie@lugatgt.org>
2014-09-12 16:56:32 -0400

    Skeleton for player classes.

---
+++ engine/Player/LocalPlayer.h
@@ -0,0 +1,55 @@
+
+// LocalPlayer.h
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "Player.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+namespace Player {
+
+/**
+ * A player connected to a local input device.
+ * @author Michael Imamura
+ */
+class LocalPlayer : public Player
+{
+	typedef Player SUPER;
+	public:
+		LocalPlayer() = delete;
+		LocalPlayer(std::shared_ptr<Profile> profile);
+		virtual ~LocalPlayer();
+};
+
+}  // namespace Player
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
---
+++ engine/Player/Player.cpp
@@ -0,0 +1,44 @@
+
+// Player.cpp
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "../StdAfx.h"
+
+#include "Player.h"
+
+namespace HoverRace {
+namespace Player {
+
+Player::Player(std::shared_ptr<Profile> profile) :
+	profile(std::move(profile)),
+	connectionState(ConnectionState::CONNECTING)
+{
+}
+
+void Player::SetConnectionState(Player::ConnectionState state)
+{
+	if (this->connectionState != state) {
+		this->connectionState = state;
+		//TODO: Fire state changed signal.
+	}
+}
+
+}  // namespace Player
+}  // namespace HoverRace
