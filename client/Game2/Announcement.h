7925dbf8cb097115c0c18466445ddf42973051a8
Michael Imamura <zoogie@lugatgt.org>
2014-09-17 19:59:57 -0400

    Iniital bits for announcements.

---
+++ client/Game2/Announcement.h
@@ -0,0 +1,86 @@
+
+// Announcement.h
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+namespace HoverRace {
+	namespace Display {
+		class Display;
+		class FillBox;
+		class FlexGrid;
+	}
+	namespace Player {
+		class Player;
+	}
+}
+
+namespace HoverRace {
+namespace Client {
+
+/**
+ * Base class for announcements.
+ * @author Michael Imamura
+ */
+class Announcement
+{
+	public:
+		/**
+		 * Constructor.
+		 * @param postType The type of announcement, used for debugging.
+		 * @param label The user-visible label.
+		 * @param player The optional target player this announcement is
+		 *               aimed at.
+		 */
+		Announcement(const std::string &postType, const std::string &label,
+			std::shared_ptr<Player::Player> player) :
+			postType(postType), label(label), player(std::move(player)) { }
+		virtual ~Announcement() { }
+
+	public:
+		const std::string &GetPostType() const { return postType; }
+		const std::string &GetLabel() const { return label; }
+		std::shared_ptr<Player::Player> GetPlayer() const { return player; }
+
+	public:
+		/**
+		 * Create the optional icon to display next to the announcement.
+		 * @param display The display the announcement will appear on.
+		 * @return The generated icon (may be @c nullptr).
+		 */
+		virtual Display::FillBox *CreateIcon(Display::Display &display) const = 0;
+
+		/**
+		 * Fill in the UI contents of the announcement.
+		 * @param display The display the announcement will appear on.
+		 * @param grid The grid (initially empty) to which components should
+		 *             be added.
+		 */
+		virtual void CreateContents(Display::Display &display,
+			Display::FlexGrid *grid) const = 0;
+
+	private:
+		const std::string postType;
+		const std::string label;
+		std::shared_ptr<Player::Player> player;
+};
+
+}  // namespace Client
+}  // namespace HoverRace
