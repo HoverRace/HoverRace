e0116628f6a9616b9c305f6a6ef3e46c9c8ec589
Michael Imamura <zoogie@lugatgt.org>
2014-10-14 08:45:45 -0400

    Add AvatarTextureRes.

---
+++ engine/Display/AvatarTextureRes.cpp
@@ -0,0 +1,61 @@
+
+// AvatarTextureRes.cpp
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "../StdAfx.h"
+
+#include <boost/filesystem/fstream.hpp>
+
+#include "../Util/Config.h"
+#include "../Util/OS.h"
+#include "../Util/Str.h"
+
+#include "AvatarTextureRes.h"
+
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Display {
+
+AvatarTextureRes::AvatarTextureRes(const std::string &avatarId) :
+	SUPER(),
+	avatarId(avatarId), id("avatar:" + avatarId)
+{
+}
+
+std::unique_ptr<std::istream> AvatarTextureRes::Open() const
+{
+	namespace fs = boost::filesystem;
+	
+	auto cfg = Util::Config::GetInstance();
+
+	//TODO: Sanity check avatar ID and return placeholder if necessary.
+	//TODO: Check if avatar cached first; schedule download if necessary.
+	
+	auto imgPath = cfg->GetMediaPath();
+	imgPath /= Str::UP("avatars");
+	imgPath /= Str::UP(avatarId + ".png");
+
+	return std::unique_ptr<std::istream>(new fs::ifstream(
+		imgPath, std::ios_base::in | std::ios_base::binary));
+}
+
+}  // namespace Display
+}  // namespace HoverRace
---
+++ engine/Display/AvatarTextureRes.h
@@ -0,0 +1,70 @@
+
+// AvatarTextureRes.h
+//
+// Copyright (c) 2014 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "Res.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+	namespace Display {
+		class Texture;
+	}
+}
+
+namespace HoverRace {
+namespace Display {
+
+/**
+ * An avatar resource.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare AvatarTextureRes : public Res<Texture>
+{
+	typedef Res<Texture> SUPER;
+
+public:
+	AvatarTextureRes(const std::string &avatarId);
+	virtual ~AvatarTextureRes() { }
+
+public:
+	std::string GetId() const override { return id; }
+
+	std::unique_ptr<std::istream> Open() const override;
+
+private:
+	const std::string avatarId;
+	const std::string id;
+};
+
+}  // namespace Display
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
