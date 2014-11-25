09dcdead8ce76a14e3df7d0aea045173431da9f9
Michael Imamura <zoogie@lugatgt.org>
2014-11-24 23:47:24 -0500

    Remove Windows-specific SetLocale() bits.
    SetThreadLocale() does nothing in Vista and later, and we don't do
    localization via resources anymore anyway.

--- engine/Util/OS.cpp
+++ engine/Util/OS.cpp
@@ -169,79 +169,6 @@ void OS::SetEnv(const char *key, const char *val)
 #	endif
 }
 
-#ifdef _WIN32
-static const char *locLang = NULL;
-static const char *locRegion = NULL;
-//static const char *locCharset = NULL;
-//static const char *locModifier = NULL;
-static bool locMatched = false;
-
-// Callback for EnumSystemLocales(), used by OS::SetLocale().
-// See EnumLocalesProc in MSDN.
-static BOOL CALLBACK SetLocaleProc(LPTSTR locale)
-{
-	char *s;
-	LCID lcid = strtoul(locale, &s, 16);
-	if (*s != '\0') return TRUE;
-
-	// Check for matching language name.
-	char curLang[16];
-	if (GetLocaleInfo(lcid, LOCALE_SISO639LANGNAME, curLang, 16) == 0) return TRUE;
-	if (strcmp(curLang, locLang) != 0) return TRUE;
-
-	// Check for matching region.
-	if (locRegion == NULL) {
-		if (SUBLANGID(LANGIDFROMLCID(lcid)) != SUBLANG_DEFAULT) return TRUE;
-	}
-	else {
-		char curRegion[16];
-		if (GetLocaleInfo(lcid, LOCALE_SISO3166CTRYNAME, curRegion, 16) == 0) return TRUE;
-		if (strcmp(curRegion, locRegion) != 0) return TRUE;
-	}
-
-	// If we made it this far, then we have a match!
-	// Note: This does nothing on Vista and later.
-	if (SetThreadLocale(lcid) == 0) {
-		// Found a matching LCID but couldn't set it.
-		/*
-		DWORD err = GetLastError();
-		LPVOID errMsg;
-		FormatMessage(
-			FORMAT_MESSAGE_ALLOCATE_BUFFER |
-			FORMAT_MESSAGE_FROM_SYSTEM,
-			NULL,
-			err,
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-			(LPTSTR)&errMsg,
-			0, NULL );
-		MessageBox(NULL, (const char*)errMsg, "AIEEE", MB_ICONERROR | MB_APPLMODAL | MB_OK);
-		LocalFree(errMsg);
-		*/
-		ASSERT(FALSE);
-	}
-
-	// Now set the global locale using the Win32-formatted name.
-	char fullLang[64];
-	if (GetLocaleInfo(lcid, LOCALE_SENGLANGUAGE, fullLang, 64) == 0) return TRUE;
-	std::string fullLocale = fullLang;
-	char fullRegion[64];
-	if (GetLocaleInfo(lcid, LOCALE_SENGCOUNTRY, fullRegion, 64) != 0) {
-		fullLocale.append("_").append(fullRegion);
-	}
-	Log::Info("Full locale name: %s", fullLocale.c_str());
-	try {
-		OS::locale = std::locale(fullLocale.c_str());
-	}
-	catch (std::runtime_error&) {
-		ASSERT(FALSE);
-		return TRUE;
-	}
-
-	locMatched = true;
-	return FALSE;
-}
-#endif
-
 /**
  * Set the locale based on the current environment.
  */
@@ -250,75 +177,13 @@ void OS::SetLocale()
 	// Common setting.
 	setlocale(LC_ALL, "");
 
-#	ifdef _WIN32
-		// For Win32, each thread has its own locale setting.
-		// We need to map the ISO-abbreviated locale name from the env
-		// to a Win32 LCID by enumerating all of the supported locales
-		// and finding the one that matches.
-
-		char *lang = getenv("LC_ALL");
-		if (lang == NULL || *lang == '\0') {
-			lang = getenv("LC_MESSAGES");
-			if (lang == NULL || *lang == '\0') {
-				lang = getenv("LANG");
-				if (lang == NULL || *lang == '\0') {
-					SetThreadLocale(LOCALE_SYSTEM_DEFAULT);
-					return;
-				}
-			}
-		}
-		lang = _strdup(lang);
-
-		if (strcmp(lang, "C") == 0) {
-			// The "C" locale on Win32 roughly means to use system default.
-			SetThreadLocale(LOCALE_SYSTEM_DEFAULT);
-		}
-		else {
-			// POSIX locale string format:
-			//   lang[_region][.charset][@modifier]
-			// lang corresponds to the LOCALE_SISO639LANGNAME and
-			// region corresponds to the LOCALE_SISO3166CTRYNAME.
-			// We currently ignore the charset and modifier.
-			locLang = lang;
-			locRegion = NULL;
-			//locCharset = NULL;
-			//locModifier = NULL;
-			for (char *s = lang; *s != '\0'; ++s) {
-				switch (*s) {
-					case '_':
-						*s = '\0';
-						locRegion = s + 1;
-						break;
-					case '.':
-						*s = '\0';
-						//locCharset = s + 1;
-						break;
-					case '@':
-						*s = '\0';
-						//locModifier = s + 1;
-						break;
-				}
-			}
-
-			locMatched = false;
-			EnumSystemLocales(SetLocaleProc, LCID_SUPPORTED);
-			if (!locMatched) {
-				Log::Warn("Unsupported locale (falling back to default).");
-				SetThreadLocale(LOCALE_SYSTEM_DEFAULT);
-				locale = std::locale("C");
-			}
-		}
-
-		free(lang);
-#	else
-		try {
-			locale = std::locale("");
-		}
-		catch (std::runtime_error&) {
-			Log::Warn("Unsupported locale (falling back to default).");
-			locale = std::locale("C");
-		}
-#	endif
+	try {
+		locale = std::locale("");
+	}
+	catch (std::runtime_error&) {
+		Log::Warn("Unsupported locale (falling back to default).");
+		locale = std::locale("C");
+	}
 
 	// Update the current locale instance.
 	std::locale::global(locale);
