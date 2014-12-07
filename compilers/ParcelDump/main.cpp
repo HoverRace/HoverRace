HoverRace/HoverRace

commit cb59a7133f4c8721fd24263f6e165a7b6892ada3

Clean up unused param.

compilers/ParcelDump/main.cpp
@@ -86,7 +86,7 @@ static void DumpTrack(const OS::path_t &path, const std::string &name)
 	InspectAndPrint("Track", track.get());
 }
 
-int main(int argc, char **argv)
+int main(int argc, char**)
 {
 #	ifdef _WIN32
 		/*

compilers/ResourceCompiler/ResourceCompiler.cpp
@@ -75,7 +75,7 @@ static void PrintUsage();
 // Local data
 static ResourceLibBuilder gsLib;
 
-int main(int pArgc, char *pArgs[])
+int main(int pArgc, char**)
 {
 	BOOL lReturnValue = TRUE;
 

commit 7255944ff763c0390d4ec0eae288590af5fb69ce

Fix Linux build caused by cb59a7133f4c8721fd24263f6e165a7b6892ada3
argv is used by the Linux build; mark it as unused only on Win32.

compilers/ParcelDump/main.cpp
@@ -1,8 +1,7 @@
 
 // main.cpp
-// Debug dumper for parcel files.
 //
-// Copyright (c) 2010 Michael Imamura.
+// Copyright (c) 2010, 2014 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
@@ -86,7 +85,7 @@ static void DumpTrack(const OS::path_t &path, const std::string &name)
 	InspectAndPrint("Track", track.get());
 }
 
-int main(int argc, char**)
+int main(int argc, char **argv)
 {
 #	ifdef _WIN32
 		/*
@@ -103,6 +102,7 @@ int main(int argc, char**)
 	if (argc < 2) return EXIT_FAILURE;
 
 #	ifdef _WIN32
+		HR_UNUSED(argv);
 		int wargc;
 		wchar_t **wargv = CommandLineToArgvW(GetCommandLineW(), &wargc);
 		OS::path_t filename(wargv[1]);

compilers/ResourceCompiler/ResourceCompiler.cpp
@@ -75,7 +75,7 @@ static void PrintUsage();
 // Local data
 static ResourceLibBuilder gsLib;
 
-int main(int pArgc, char**)
+int main(int pArgc, char **pArgs)
 {
 	BOOL lReturnValue = TRUE;
 
@@ -92,6 +92,7 @@ int main(int pArgc, char**)
 #	endif
 
 #	ifdef _WIN32
+		HR_UNUSED(pArgs);
 		int wargc;
 		wchar_t **wargv = CommandLineToArgvW(GetCommandLineW(), &wargc);
 #	endif