6c8b83a0755b5ac681f04dc741877411962a3fc4
Michael Imamura <zoogie@lugatgt.org>
2015-01-01 11:47:53 -0500

    Update copyright.
    Yay 2015 :)

--- client/Game2/TestLabScene.cpp
+++ client/Game2/TestLabScene.cpp
@@ -1,7 +1,7 @@
 
 // TestLabScene.cpp
 //
-// Copyright (c) 2013, 2014 Michael Imamura.
+// Copyright (c) 2013-2015 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
