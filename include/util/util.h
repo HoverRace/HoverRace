def2c10584764ccea0ae60627aee0ba337ba706d
Michael Imamura <zoogie@lugatgt.org>
2015-01-03 16:19:12 -0500

    Add HR_DEPRECATED macro.

--- include/util/util.h
+++ include/util/util.h
@@ -5,3 +5,11 @@
 
 // Mark unused parameters so we can keep both the compiler and Doxygen happy.
 template<class... T> void HR_UNUSED(T&&...) { }
+
+// Mark functions as deprecated.
+// Using compiler-specific methods until [[deprecated]] is widely supported.
+#ifdef _WIN32
+#	define HR_DEPRECATED __declspec(deprecated)
+#else
+#	define HR_DEPRECATED __attribute__((deprecated))
+#endif
