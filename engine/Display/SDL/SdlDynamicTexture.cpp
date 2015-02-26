ebd86aa8a56376dbf3c7f988f463d864263671d8
Michael Imamura <zoogie@lugatgt.org>
2015-02-25 19:49:13 -0500

    Add SdlDynamicTexture.

---
+++ engine/Display/SDL/SdlDynamicTexture.cpp
@@ -0,0 +1,95 @@
+
+// SdlDynamicTexture.cpp
+//
+// Copyright (c) 2015 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include "../../Util/MR_Types.h"
+#include "../../Exception.h"
+
+#include "SdlDynamicTexture.h"
+
+namespace HoverRace {
+namespace Display {
+namespace SDL {
+
+namespace {
+
+/**
+ * Initializes the surface based on an existing texture.
+ * @param texture The texture (may not be @c nullptr).
+ * @return @c nullptr if it fails, else a SDL_Surface*.
+ */
+SDL_Surface *InitSurface(SDL_Texture *texture)
+{
+	if (!texture) {
+		throw Exception("Cannot create dynamic null texture.");
+	}
+
+	MR_UInt32 fmt;
+	int w, h;
+	if (SDL_QueryTexture(texture, &fmt, nullptr, &w, &h) < 0) {
+		throw DynamicTextureExn(
+			std::string("Failed to query source for dynamic texture: ") +
+			SDL_GetError());
+	}
+
+	int bpp;
+	MR_UInt32 rm, gm, bm, am;
+	if (SDL_PixelFormatEnumToMasks(fmt, &bpp, &rm, &gm, &bm, &am) == SDL_FALSE)
+	{
+		throw DynamicTextureExn(
+			std::string("Invalid pixel format for dynamic texture: ") +
+			SDL_GetError());
+	}
+	
+	SDL_Surface *retv;
+	if ((retv = SDL_CreateRGBSurface(0, w, h, bpp, rm, gm, bm, am)) == nullptr)
+	{
+		throw DynamicTextureExn(
+			std::string("Failed to create surface for dynamic texture: ") +
+			SDL_GetError());
+	}
+
+	return retv;
+}
+
+}  // namespace
+
+/**
+ * Constructor.
+ * @param display The display.
+ * @param texture The texture (may not be @c nullptr).
+ * @throws DynamicTextureExn Could not be created
+ *                           (@p texture will still be freed).
+ */
+SdlDynamicTexture::SdlDynamicTexture(SdlDisplay &display,
+	SDL_Texture *texture) :
+	SUPER(display, texture)
+{
+	try {
+		surface = InitSurface(texture);
+	}
+	catch (...) {
+		if (texture) SDL_DestroyTexture(texture);
+	}
+}
+
+}  // namespace SDL
+}  // namespace Display
+}  // namespace HoverRace
---
+++ engine/Display/SDL/SdlDynamicTexture.h
@@ -0,0 +1,112 @@
+
+// SdlDynamicTexture.h
+//
+// Copyright (c) 2015 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "SdlTexture.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+	namespace Display {
+		namespace SDL {
+			class SdlDisplay;
+		}
+	}
+}
+
+namespace HoverRace {
+namespace Display {
+namespace SDL {
+
+/**
+ * Wrapper for an SDL texture that can be updated.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare SdlDynamicTexture : public SdlTexture
+{
+	using SUPER = SdlTexture;
+
+public:
+	SdlDynamicTexture() = delete;
+	SdlDynamicTexture(SdlDisplay &display, SDL_Texture *texture);
+	SdlDynamicTexture(const SdlDynamicTexture&) = delete;
+
+	virtual ~SdlDynamicTexture()
+	{
+		if (surface) SDL_FreeSurface(surface);
+	}
+
+	SdlDynamicTexture &operator=(const SdlDynamicTexture&) = delete;
+
+public:
+	/**
+	 * Updates the texture from the backing surface.
+	 */
+	void Update()
+	{
+		SDL_UpdateTexture(texture, nullptr, surface->pixels, surface->pitch);
+	}
+
+	/**
+	 * Updates the texture with new pixel data.
+	 * @param fn The update function.  Will be passed the drawing surface.
+	 *           Returns @c true to update the texture immediately,
+	 *           or @c false to defer the update until a later call to Update().
+	 */
+	template<class Fn>
+	void Update(Fn fn)
+	{
+		if (fn(surface)) Update();
+	}
+
+protected:
+	SDL_Surface *surface;
+};
+
+/**
+ * Thrown when the dynamic texture could not be created.
+ * @author Michael Imamura
+ */
+class DynamicTextureExn : public Exception
+{
+	typedef Exception SUPER;
+
+public:
+	DynamicTextureExn() : SUPER() { }
+	DynamicTextureExn(const std::string &msg) : SUPER(msg) { }
+	DynamicTextureExn(const char *msg) : SUPER(msg) { }
+	virtual ~DynamicTextureExn() noexcept { }
+};
+
+}  // namespace SDL
+}  // namespace Display
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
