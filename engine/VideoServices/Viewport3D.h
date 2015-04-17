147a3e4109328edba259a13069cb852bec4e01d4
Michael Imamura <zoogie@lugatgt.org>
2015-04-17 17:33:08 -0400

    Reformat.

--- engine/VideoServices/Viewport3D.h
+++ engine/VideoServices/Viewport3D.h
@@ -46,85 +46,84 @@ namespace VideoServices {
 // Helper class
 class PositionMatrix
 {
-	public:
-		MR_Int32 mRotation[2][2];
-		MR_3DCoordinate mDisplacement;
+public:
+	MR_Int32 mRotation[2][2];
+	MR_3DCoordinate mDisplacement;
 };
 
 class Viewport3D : public Viewport2D
 {
-	protected:
-
-		class BackColumn
-		{
-			public:
-				MR_Int32 mBitmapColumn;
-				MR_Int32 mLineIncrement_1024;
-		};
-
-		//Camera setting
-		MR_3DCoordinate mPosition;
-		MR_Angle mOrientation;					  // Angle from the XAxis
-		int mScroll;							  // Offset to YCenter
-		MR_Angle mVAngle;						  // Vertical field angle width
-		MR_Int32 mPlanDist;						  // ProjectionPlan (screen) distance
-		MR_Int32 mPlanHW;						  // Horizontal Half
-		MR_Int32 mPlanVW;						  // Vertical Half
-
-		MR_UInt16 *mZBuffer;
-		int mZLineLen;
-
-		MR_UInt8 **mBufferLine;
-		MR_UInt16 **mZBufferLine;
-
-		// Usefull pre-defined constants
-		MR_Int32 mHVarPerDInc_16384;			  // Ray divergence by HPixel
-		MR_Int32 mVVarPerDInc_16384;			  // Ray divergence by VPixel
-		MR_Int32 mXRes_PlanDist;
-		MR_Int32 mYRes_PlanDist;
-
-		MR_Int32 mPlanHW_PlanDist_2_XRes_16384;	  // 16384*mPlanHW/(mPlanDist*2*mXRes)
-		MR_Int32 mXRes_PlanDist_2PlanHW_4096;	  // mXRes*mPlanDist*2048/mPlanHW
-
-		BackColumn *mBackgroundConst;			  // Constants used to display each bitmap column
-
-		MR_Int32 mRotationMatrix[3][3];
-
-		void ComputeRotationMatrix();
-		void ComputeBackgroundConst();
-
-		void ApplyRotationMatrix(const MR_3DCoordinate & pSrc, MR_3DCoordinate & pDest) const;
-		void ApplyRotationMatrix(const MR_2DCoordinate & pSrc, MR_2DCoordinate & pDest) const;
-		void ApplyPositionMatrix(const PositionMatrix & pMatrix, const MR_3DCoordinate & pSrc, MR_3DCoordinate & pDest) const;
-
-		MR_DllDeclare void OnMetricsChange(int pMetrics);
-
+protected:
+	class BackColumn
+	{
 	public:
+		MR_Int32 mBitmapColumn;
+		MR_Int32 mLineIncrement_1024;
+	};
 
-		MR_DllDeclare Viewport3D();
-		MR_DllDeclare ~Viewport3D();
+	//Camera setting
+	MR_3DCoordinate mPosition;
+	MR_Angle mOrientation;					  // Angle from the XAxis
+	int mScroll;							  // Offset to YCenter
+	MR_Angle mVAngle;						  // Vertical field angle width
+	MR_Int32 mPlanDist;						  // ProjectionPlan (screen) distance
+	MR_Int32 mPlanHW;						  // Horizontal Half
+	MR_Int32 mPlanVW;						  // Vertical Half
 
-		MR_DllDeclare void Setup(VideoBuffer *pBuffer, int pX0, int pY0, int pSizeX, int pSizeY, MR_Angle pApperture, int pMetrics = eNone);
+	MR_UInt16 *mZBuffer;
+	int mZLineLen;
 
-		MR_DllDeclare void SetupCameraPosition(const MR_3DCoordinate & pPosition, MR_Angle pOrientation, int pScroll);
+	MR_UInt8 **mBufferLine;
+	MR_UInt16 **mZBufferLine;
 
-		MR_DllDeclare void ClearZ();
+	// Usefull pre-defined constants
+	MR_Int32 mHVarPerDInc_16384;			  // Ray divergence by HPixel
+	MR_Int32 mVVarPerDInc_16384;			  // Ray divergence by VPixel
+	MR_Int32 mXRes_PlanDist;
+	MR_Int32 mYRes_PlanDist;
 
-		MR_DllDeclare BOOL ComputePositionMatrix(PositionMatrix & pMatrix, const MR_3DCoordinate & pPosition, MR_Angle pOrientation, MR_Int32 pMaxObjRay);
+	MR_Int32 mPlanHW_PlanDist_2_XRes_16384;	  // 16384*mPlanHW/(mPlanDist*2*mXRes)
+	MR_Int32 mXRes_PlanDist_2PlanHW_4096;	  // mXRes*mPlanDist*2048/mPlanHW
 
-		// WireFrame services
-		MR_DllDeclare void DrawWFLine(const MR_3DCoordinate & pP0, const MR_3DCoordinate & pP1, MR_UInt8 pColor);
+	BackColumn *mBackgroundConst;			  // Constants used to display each bitmap column
 
-		// Rendering services ( availlable in the file 3DViewportRendering.cpp )
-		MR_DllDeclare void RenderWallSurface(const MR_3DCoordinate & pUpperLeft, const MR_3DCoordinate & pLowerRight, MR_Int32 pLen, const Bitmap * pBitmap);
-		MR_DllDeclare void RenderAlternateWallSurface(const MR_3DCoordinate & pUpperLeft, const MR_3DCoordinate & pLowerRight, MR_Int32 pLen, const Bitmap * pBitmap, const Bitmap * pBitmap2, int pSerialLen, int pSerialStart);
+	MR_Int32 mRotationMatrix[3][3];
 
-		MR_DllDeclare void RenderHorizontalSurface(int lNbVertex, const MR_2DCoordinate * pVertexList, MR_Int32 pLevel, BOOL lTop, const Bitmap * pBitmap);
+	void ComputeRotationMatrix();
+	void ComputeBackgroundConst();
 
-		MR_DllDeclare void RenderPatch(const Patch & pPatch, const PositionMatrix & pMatrix, const Bitmap * pBitmap);
-		MR_DllDeclare void RenderPatch(const Patch & pPatch, const PositionMatrix & pMatrix, MR_UInt8 pColor);
+	void ApplyRotationMatrix(const MR_3DCoordinate & pSrc, MR_3DCoordinate & pDest) const;
+	void ApplyRotationMatrix(const MR_2DCoordinate & pSrc, MR_2DCoordinate & pDest) const;
+	void ApplyPositionMatrix(const PositionMatrix & pMatrix, const MR_3DCoordinate & pSrc, MR_3DCoordinate & pDest) const;
 
-		MR_DllDeclare void RenderBackground(const MR_UInt8 * pBitmap);
+	MR_DllDeclare void OnMetricsChange(int pMetrics);
+
+public:
+
+	MR_DllDeclare Viewport3D();
+	MR_DllDeclare ~Viewport3D();
+
+	MR_DllDeclare void Setup(VideoBuffer *pBuffer, int pX0, int pY0, int pSizeX, int pSizeY, MR_Angle pApperture, int pMetrics = eNone);
+
+	MR_DllDeclare void SetupCameraPosition(const MR_3DCoordinate & pPosition, MR_Angle pOrientation, int pScroll);
+
+	MR_DllDeclare void ClearZ();
+
+	MR_DllDeclare BOOL ComputePositionMatrix(PositionMatrix & pMatrix, const MR_3DCoordinate & pPosition, MR_Angle pOrientation, MR_Int32 pMaxObjRay);
+
+	// WireFrame services
+	MR_DllDeclare void DrawWFLine(const MR_3DCoordinate & pP0, const MR_3DCoordinate & pP1, MR_UInt8 pColor);
+
+	// Rendering services ( availlable in the file 3DViewportRendering.cpp )
+	MR_DllDeclare void RenderWallSurface(const MR_3DCoordinate & pUpperLeft, const MR_3DCoordinate & pLowerRight, MR_Int32 pLen, const Bitmap * pBitmap);
+	MR_DllDeclare void RenderAlternateWallSurface(const MR_3DCoordinate & pUpperLeft, const MR_3DCoordinate & pLowerRight, MR_Int32 pLen, const Bitmap * pBitmap, const Bitmap * pBitmap2, int pSerialLen, int pSerialStart);
+
+	MR_DllDeclare void RenderHorizontalSurface(int lNbVertex, const MR_2DCoordinate * pVertexList, MR_Int32 pLevel, BOOL lTop, const Bitmap * pBitmap);
+
+	MR_DllDeclare void RenderPatch(const Patch & pPatch, const PositionMatrix & pMatrix, const Bitmap * pBitmap);
+	MR_DllDeclare void RenderPatch(const Patch & pPatch, const PositionMatrix & pMatrix, MR_UInt8 pColor);
+
+	MR_DllDeclare void RenderBackground(const MR_UInt8 * pBitmap);
 };
 
 // Local constants (Used by the cpp of this module)
