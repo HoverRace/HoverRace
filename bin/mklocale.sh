c14500f24f96246653ab7b21ce521dcef4c5fa0a
Michael Imamura <zoogie@lugatgt.org>
2015-09-23 17:44:34 -0400

    Modernize a bit.

--- bin/mklocale.sh
+++ bin/mklocale.sh
@@ -7,10 +7,9 @@ mkdir locale
 for lingua in "$@"; do
 	lingua="$(basename "$lingua" .gmo)"
 	gmofile="../po/$lingua.gmo"
-	if [ -f "$gmofile" ]; then
+	if [[ -f "$gmofile" ]]; then
 		echo -n "$lingua "
-		mkdir "locale/$lingua"
-		mkdir "locale/$lingua/LC_MESSAGES"
+		mkdir -p "locale/$lingua/LC_MESSAGES" || exit 1
 		ln -s \
 			"../../../$gmofile" \
 			"locale/$lingua/LC_MESSAGES/hoverrace.mo"
