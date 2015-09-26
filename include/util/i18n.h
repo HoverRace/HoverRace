3ab907527342f5ba2cdb71513afe370f67d76988
Michael Imamura <zoogie@lugatgt.org>
2015-09-26 02:08:44 -0400

    Always include Boost.Locale.
    Even when ENABLE_NLS is 0, we still need Boost.Locale for
    non-translation bits.

--- include/util/i18n.h
+++ include/util/i18n.h
@@ -3,29 +3,17 @@
 
 #pragma once
 
+#ifdef _WIN32
+#	pragma warning(push, 0)
+#endif
+
+#include <boost/locale.hpp>
+
+#ifdef _WIN32
+#	pragma warning(pop)
+#endif
+
 #ifdef _
 #	undef _
 #endif
-#ifdef ENABLE_NLS
-#	ifdef _WIN32
-#		pragma warning(push, 0)
-#	endif
-
-#	include <boost/locale.hpp>
-
-#	ifdef _WIN32
-#		pragma warning(pop)
-#	endif
-
-#	define _(...) ::boost::locale::translate(__VA_ARGS__)
-
-#else
-
-#	define _(x) (x)
-#	define gettext(x) (x)
-#	define dgettext(d,x) (x)
-#	define dcgettext(d,x,c) (x)
-#	define pgettext(p,x) (x)
-#	define pgettextImpl(f,x) (x)
-
-#endif  // ENABLE_NLS
+#define _(...) ::boost::locale::translate(__VA_ARGS__)
