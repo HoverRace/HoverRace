918186d36303dbcf3d19be6a623cef42873d0863
Michael Imamura <zoogie@lugatgt.org>
2015-10-21 22:51:25 -0400

    Remove "POT-Creation-Date" line after xgettext.
    This currently only works when the _potFile_1 target is actually run; if
    the "translations" target is run instead, then hoverrace.pot is still
    generated but the CleanPot.cmake script isn't triggered.

---
+++ po/CleanPot.cmake
@@ -0,0 +1,8 @@
+
+set(POT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/hoverrace.pot)
+
+message(STATUS "Cleaning up ${POT_FILE}")
+
+file(READ "${POT_FILE}" pot_in)
+string(REGEX REPLACE "\"POT-Creation-Date:[^\r\n]*\r?\n" "" pot_out "${pot_in}")
+file(WRITE "${POT_FILE}" "${pot_out}")
