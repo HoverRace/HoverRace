f3ec2a8e4572137ad580e5c11a45b1704b2c610b
Michael Imamura <zoogie@lugatgt.org>
2015-10-25 12:26:46 -0400

    Document our cleanup step.

--- po/CleanPot.cmake
+++ po/CleanPot.cmake
@@ -4,5 +4,9 @@ set(POT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/po/hoverrace.pot)
 message(STATUS "Cleaning up ${POT_FILE}")
 
 file(READ "${POT_FILE}" pot_in)
+
+# Remove the creation date header since the .pot file is regenerated during
+# the build, and the updated header propagates to the .po files.
 string(REGEX REPLACE "\"POT-Creation-Date:[^\r\n]*\r?\n" "" pot_out "${pot_in}")
+
 file(WRITE "${POT_FILE}" "${pot_out}")
