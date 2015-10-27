91416754d09bbf8b9322b1ba1e9b02510b050259
Michael Imamura <zoogie@lugatgt.org>
2015-10-26 20:11:49 -0400

    Fix case of multiple filenames on one line.
    xgettext will try to put as many filenames in a comment line as
    possible, and the wrap settings don't seem to affect it.
    
    This adds a pass where we look for those cases and split them into
    separate comment lines.

--- po/CleanPo.cmake
+++ po/CleanPo.cmake
@@ -5,7 +5,11 @@ message(STATUS "Cleaning up ${PO_FILE}")
 
 file(READ "${PO_FILE}" po_in)
 
-#TODO: If a comment has more than one filename, split into multiple lines.
+# If a comment has more than one filename, split into multiple lines.
+# We assume that there are at most two filenames on a single line.
+string(REGEX REPLACE
+	"(#: [^ \r\n]+) ([^\r\n]+)(\r?\n)" "\\1\\3#: \\2\\3"
+	po_split "${po_in}")
 
 # Remove the line number from the comment, leaving only the filename.
 # The line numbers change often so they clutter the history.
@@ -13,8 +17,7 @@ file(READ "${PO_FILE}" po_in)
 # 0.18 compatibility, so we'll do it ourselves.
 string(REGEX REPLACE
 	"(#: [^:]+):[0-9]+([^\r\n]*\r?\n)" "\\1\\2"
-	po_out "${po_in}")
-
+	po_out "${po_split}")
 
 file(WRITE "${PO_FILE}" "${po_out}")
 
