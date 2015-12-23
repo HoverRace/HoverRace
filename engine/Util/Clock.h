4e67cc6b77fe67730b46fa28922bc7d180134d1b
Michael Imamura <zoogie@lugatgt.org>
2015-12-23 16:48:55 -0500

    Reformat.

--- engine/Util/Clock.h
+++ engine/Util/Clock.h
@@ -1,7 +1,7 @@
 
 // Clock.h
 //
-// Copyright (c) 2013, 2014 Michael Imamura.
+// Copyright (c) 2013-2015 Michael Imamura.
 //
 // Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
 // you may not use this file except in compliance with the License.
@@ -43,73 +43,73 @@ namespace Util {
  */
 class MR_DllDeclare Clock : public std::enable_shared_from_this<Clock>
 {
-	public:
-		Clock();
-		Clock(const Duration &init);
+public:
+	Clock();
+	Clock(const Duration &init);
 
-		// We can't copy the alarm signals, so we can't be copyable either.
-		Clock(const Clock&) = delete;
-		Clock(Clock&&) = default;
-		Clock &operator=(const Clock&) = delete;
-		Clock &operator=(Clock&&) = default;
+	// We can't copy the alarm signals, so we can't be copyable either.
+	Clock(const Clock&) = delete;
+	Clock(Clock&&) = default;
+	Clock &operator=(const Clock&) = delete;
+	Clock &operator=(Clock&&) = default;
 
-		virtual ~Clock() { }
+	virtual ~Clock() { }
 
-	public:
-		std::string FmtLong() const { return lastRead.FmtLong(); }
-		std::string FmtShort() const { return lastRead.FmtShort(); }
+public:
+	std::string FmtLong() const { return lastRead.FmtLong(); }
+	std::string FmtShort() const { return lastRead.FmtShort(); }
 
-	public:
-		/**
-		 * Retrieve the time of the last call to Advance().
-		 */
-		const Duration &GetTime() const { return lastRead; }
-		void SetTime(const Duration &duration=Duration());
+public:
+	/**
+	 * Retrieve the time of the last call to Advance().
+	 */
+	const Duration &GetTime() const { return lastRead; }
+	void SetTime(const Duration &duration=Duration());
 
-		Duration Advance();
+	Duration Advance();
 
-	public:
-		typedef boost::signals2::signal<void()> alarmSignal_t;
+public:
+	using alarmSignal_t =  boost::signals2::signal<void()>;
 
-		/**
-		 * Remove all alarms.
-		 */
-		void ClearAlarms()
-		{
-			alarms.clear();
+	/**
+	 * Remove all alarms.
+	 */
+	void ClearAlarms()
+	{
+		alarms.clear();
+	}
+
+	/**
+	 * Set a one-shot alarm to run at a specific time.
+	 *
+	 * It's not guaranteed that the alarm will be fired at the exact time
+	 * since alarms are only fired when Advance() is called; however,
+	 * an alarm will never be fired before it's timestamp.
+	 *
+	 * @param duration The clock timestamp.
+	 * @param fn The function to invoke.
+	 * @return The signal connection; use this to cancel the alarm by
+	 *         disconnecting it.
+	 */
+	template<class Fn>
+	boost::signals2::connection At(const Duration &duration, Fn &&fn)
+	{
+		auto &sig = alarms[duration];
+		if (!sig) {
+			sig.reset(new alarmSignal_t());
 		}
+		return sig->connect(std::forward<Fn>(fn));
+	}
 
-		/**
-		 * Set a one-shot alarm to run at a specific time.
-		 *
-		 * It's not guaranteed that the alarm will be fired at the exact time
-		 * since alarms are only fired when Advance() is called; however,
-		 * an alarm will never be fired before it's timestamp.
-		 *
-		 * @param duration The clock timestamp.
-		 * @param fn The function to invoke.
-		 * @return The signal connection; use this to cancel the alarm by
-		 *         disconnecting it.
-		 */
-		template<class Fn>
-		boost::signals2::connection At(const Duration &duration, Fn &&fn)
-		{
-			auto &sig = alarms[duration];
-			if (!sig) {
-				sig.reset(new alarmSignal_t());
-			}
-			return sig->connect(std::forward<Fn>(fn));
-		}
+private:
+	Duration lastRead;
+	Duration start;
+	Duration offset;
 
-	private:
-		Duration lastRead;
-		Duration start;
-		Duration offset;
-
-		// Sorted list of pending alarms.
-		// We need to wrap the signal in a unique_ptr because depending on the
-		// Boost version, it may not have move semantics.
-		std::map<Duration, std::unique_ptr<alarmSignal_t>> alarms;
+	// Sorted list of pending alarms.
+	// We need to wrap the signal in a unique_ptr because depending on the
+	// Boost version, it may not have move semantics.
+	std::map<Duration, std::unique_ptr<alarmSignal_t>> alarms;
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Clock &clock)
