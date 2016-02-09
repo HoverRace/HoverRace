1456ebd0e74369d4a49136372f908614063f8383
Michael Imamura <zoogie@lugatgt.org>
2016-02-08 20:28:21 -0500

    Add ProfileGallery.

---
+++ engine/Player/ProfileGallery.cpp
@@ -0,0 +1,108 @@
+
+// ProfileGallery.cpp
+//
+// Copyright (c) 2016 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include <boost/filesystem/convenience.hpp>
+#include <boost/uuid/string_generator.hpp>
+
+#include "../Util/Config.h"
+#include "../Util/Log.h"
+#include "../Util/Str.h"
+#include "LocalProfile.h"
+#include "ProfileExn.h"
+
+#include "ProfileGallery.h"
+
+namespace fs = boost::filesystem;
+namespace uuid = boost::uuids;
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Player {
+
+/**
+ * Constructor.
+ * @param path The base profile directory (may be empty to use default).
+ * @throw ProfileExn The full list of profiles could not be loaded.
+ */
+ProfileGallery::ProfileGallery(const OS::path_t &path) :
+	path(path)
+{
+	if (path.empty()) {
+		this->path = Config::GetInstance()->GetProfilePath();
+	}
+
+	Reload();
+}
+
+/**
+ * Reload all profiles.
+ * @throw ProfileExn The full list of profiles could not be loaded.
+ */
+void ProfileGallery::Reload()
+{
+	profiles.clear();
+
+	if (!fs::exists(path)) {
+		HR_LOG(info) << "Profile directory does not exist: " <<
+			(const char*)Str::PU(path);
+		return;
+	}
+
+	if (!fs::is_directory(path)) {
+		throw ProfileExn("Profile path is not a directory: " +
+			(const std::string&)Str::PU(path));
+	}
+
+	uuid::string_generator uuidGen;
+	OS::dirIter_t dend;
+	try {
+		HR_LOG(info) << "Loading profiles: " << (const char*)Str::PU(path);
+		for (OS::dirIter_t iter{ path }; iter != dend; ++iter) {
+			std::string uidStr = iter->path().filename().string();
+
+			uuid::uuid uid;
+			try {
+				uid = uuidGen(uidStr);
+			}
+			catch (...) {
+				HR_LOG(warning) << "Profile directory is not a UUID: " <<
+					(const char*)Str::PU(path);
+				continue;
+			}
+
+			HR_LOG(debug) << "Found profile: " << uidStr;
+			try {
+				profiles.emplace_back(std::make_shared<LocalProfile>(uid));
+			}
+			catch (const ProfileExn &ex) {
+				HR_LOG(error) << "Failed to load profile: " << uidStr << ": " <<
+					ex.what();
+			}
+		}
+	}
+	catch (const std::exception &ex) {
+		throw ProfileExn(std::string("Unable to load all profiles: ") +
+			ex.what());
+	}
+}
+
+}  // namespace Player
+}  // namespace HoverRace
---
+++ engine/Player/ProfileGallery.h
@@ -0,0 +1,74 @@
+
+// ProfileGallery.h
+//
+// Copyright (c) 2016 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "../Util/OS.h"
+#include "Profile.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+namespace Player {
+
+/**
+ * The collection of player profiles on this system.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare ProfileGallery
+{
+	using SUPER = Profile;
+
+public:
+	ProfileGallery(const Util::OS::path_t &path = {});
+
+public:
+	void Reload();
+
+private:
+	using profiles_t = std::vector<std::shared_ptr<Profile>>;
+public:
+	// STL-like iteration, so we can use range-for.
+	using iterator = profiles_t::iterator;
+	using const_iterator = profiles_t::const_iterator;
+	using value_type = profiles_t::value_type;
+	const_iterator begin() const { return profiles.begin(); }
+	iterator begin() { return profiles.begin(); }
+	const_iterator end() const { return profiles.end(); }
+	iterator end() { return profiles.end(); }
+
+private:
+	Util::OS::path_t path;
+	profiles_t profiles;
+};
+
+}  // namespace Player
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
