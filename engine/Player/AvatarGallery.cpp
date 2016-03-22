f2c6d206f60387b1315e00b6bfef450d2c79bd09
Michael Imamura <zoogie@lugatgt.org>
2016-03-21 20:31:11 -0400

    Initial bits for common avatar handling.

---
+++ engine/Player/AvatarGallery.cpp
@@ -0,0 +1,83 @@
+
+// AvatarGallery.cpp
+//
+// Copyright (c) 2016 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#include <boost/filesystem/convenience.hpp>
+
+#include "../Display/MediaRes.h"
+#include "../Display/Texture.h"
+#include "../Util/Config.h"
+#include "../Util/Log.h"
+#include "../Util/Str.h"
+
+#include "AvatarGallery.h"
+
+namespace fs = boost::filesystem;
+using namespace HoverRace::Util;
+
+namespace HoverRace {
+namespace Player {
+
+/**
+ * Constructor.
+ * @param path The base avatar directory (may be empty to use default).
+ */
+AvatarGallery::AvatarGallery(const OS::path_t &path) :
+	path(path)
+{
+	if (path.empty()) {
+		this->path = Config::GetInstance()->GetMediaPath() / "avatars";
+	}
+
+	Reload();
+}
+
+/**
+ * Reload all avatars.
+ */
+void AvatarGallery::Reload()
+{
+	avatars.clear();
+
+	if (!fs::exists(path)) {
+		HR_LOG(info) << "Avatar directory does not exist: " <<
+			(const char*)Str::PU(path);
+		return;
+	}
+
+	if (!fs::is_directory(path)) {
+		HR_LOG(warning) << "Avatar directory is not a directory: " <<
+			(const char*)Str::PU(path);
+		return;
+	}
+
+	OS::dirIter_t dend;
+	for (OS::dirIter_t iter{ path }; iter != dend; ++iter) {
+		std::string avatarName = iter->path().filename().string();
+
+		HR_LOG(debug) << "Found avatar: " << avatarName;
+		avatars.emplace_back(
+			std::make_shared<Display::MediaRes<Display::Texture>>(
+				Str::UP("avatars") / avatarName));
+	}
+}
+
+}  // namespace Player
+}  // namespace HoverRace
---
+++ engine/Player/AvatarGallery.h
@@ -0,0 +1,81 @@
+
+// AvatarGallery.h
+//
+// Copyright (c) 2016 Michael Imamura.
+//
+// Licensed under GrokkSoft HoverRace SourceCode License v1.0(the "License");
+// you may not use this file except in compliance with the License.
+//
+// A copy of the license should have been attached to the package from which
+// you have taken this file. If you can not find the license you can not use
+// this file.
+//
+//
+// The author makes no representations about the suitability of
+// this software for any purpose.  It is provided "as is" "AS IS",
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.
+//
+// See the License for the specific language governing permissions
+// and limitations under the License.
+
+#pragma once
+
+#include "../Display/Res.h"
+
+#if defined(_WIN32) && defined(HR_ENGINE_SHARED)
+#	ifdef MR_ENGINE
+#		define MR_DllDeclare   __declspec( dllexport )
+#	else
+#		define MR_DllDeclare   __declspec( dllimport )
+#	endif
+#else
+#	define MR_DllDeclare
+#endif
+
+namespace HoverRace {
+	namespace Display {
+		class Texture;
+	}
+}
+
+namespace HoverRace {
+namespace Player {
+
+/**
+ * The collection of built-in avatars.
+ * @author Michael Imamura
+ */
+class MR_DllDeclare AvatarGallery
+{
+public:
+	AvatarGallery(const Util::OS::path_t &path = {});
+
+public:
+	void Reload();
+
+public:
+	bool IsEmpty() const { return avatars.empty(); }
+
+private:
+	using avatars_t =
+		std::vector<std::shared_ptr<Display::Res<Display::Texture>>>;
+public:
+	// STL-like iteration, so we can use range-for.
+	using iterator = avatars_t::iterator;
+	using const_iterator = avatars_t::const_iterator;
+	using value_type = avatars_t::value_type;
+	const_iterator begin() const { return avatars.begin(); }
+	iterator begin() { return avatars.begin(); }
+	const_iterator end() const { return avatars.end(); }
+	iterator end() { return avatars.end(); }
+
+private:
+	Util::OS::path_t path;
+	avatars_t avatars;
+};
+
+}  // namespace Player
+}  // namespace HoverRace
+
+#undef MR_DllDeclare
