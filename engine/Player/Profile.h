caf9ff03344c3329c7ab2aec6f6a807104bedacf
Michael Imamura <zoogie@lugatgt.org>
2016-06-20 19:20:06 -0400

    Change MAX_NAME_LENGTH to size_t.
    Also add documentation indicating that it is in Unicode code points.

--- engine/Player/Profile.h
+++ engine/Player/Profile.h
@@ -134,7 +134,11 @@ protected:
 	virtual void Save() = 0;
 
 public:
-	static const int MAX_NAME_LENGTH = 30;
+	/**
+	 * Maximum length for the name.
+	 * Length is in Unicode code points (not bytes!).
+	 */
+	static const size_t MAX_NAME_LENGTH = 30;
 private:
 	std::shared_ptr<AvatarGallery> avatarGallery;
 	boost::uuids::uuid uid;
